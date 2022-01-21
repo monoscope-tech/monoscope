b7f22c02de236218db1bd141618bfdb3a3832c7e
Victory Okhormhe <okhormhevictory@gmail.com>
2022-01-21 22:35:32 +0100

    Update EndpointDetails.hs

--- src/Pages/Endpoints/EndpointDetails.hs
+++ src/Pages/Endpoints/EndpointDetails.hs
@@ -16,12 +16,12 @@ import Text.RawString.QQ
 
 endpointDetails :: Html ()
 endpointDetails = bodyWrapper "Endpoint Details" $ do
-  div_ $ do
-    div_ $ do
-      span_ "GET /users/acc_details"
-      img_ [src_ "/assets/svgs/down_caret.svg"]
-    div_ $ do
-      button_ [class_ "bg-white "] $ img_ [src_ "settings.svg"]
-      a_ [href_ "#", class_ ""] $ do
+  div_ [class_ "flex flex-row justify-between"]$ do
+    div_ [class_ "flex flex-row"] $ do
+      h3_ [class_ "text-xl text-slate-700"] "GET /users/acc_details"
+      img_ [src_ "/assets/svgs/down_chevron.svg"]
+    div_ [class_ "flex flex-row"] $ do
+      button_ [class_ "bg-white "] $ img_ [src_ "/assets/svgs/filter.svg"]
+      a_ [href_ "#", class_ "flex flex-row"] $ do
         span_ "Download Swagger"
         img_ [src_ "/assets/svgs/down_chevron.svg", class_ "p-2 bg-navy"]
