d7a9e45dacb20f134d30e337e442260fce52eaae
Victory Okhormhe <okhormhevictory@gmail.com>
2022-01-25 23:32:14 +0100

    endpoint details

--- src/Pages/Endpoints/EndpointDetails.hs
+++ src/Pages/Endpoints/EndpointDetails.hs
@@ -27,30 +27,37 @@ endpointDetails = bodyWrapper "Endpoint Details" $ do
             img_ [src_ "/assets/svgs/filter.svg", class_ " h-6 w-6 m-2"] 
         a_ [href_ ""] $ do
           button_ [class_ "bg-blue-700 flex h-11 flex-row m-2 px-3 rounded-xl py-2"] $ do
-            h3_ [class_ "text-white text-sm text-bold mx-2 mt-1"] "Download JSON"
+            h3_ [class_ "text-white text-sm text-bold mx-2 mt-1"] "Download Swagger"
             div_ [class_ "bg-blue-900 p-1 rounded-lg ml-2"] $ do
               img_ [src_ "/assets/svgs/whitedown.svg", class_ "text-white h-2 w-2 m-1"] 
-    div_ [class_ "bg-amber-100 mt-3 relative rounded-xl w-full border border-yellow-400 p-8 flex "] $ do
-      div_ [class_ "mx-2"] $ do
-        img_ [src_ "/assets/svgs/warning.svg", class_ "mt-3"]
-      div_ [class_ " mx-6 border-r border-yellow-400 p-5"] $ do
-        h5_ [class_ "text-black text-neutral-800 mb-4 -mt-8 text-base"] "Added Fields (3)"
-        p_ [href_ "", class_ "text-sm underline underline-offset-1"] "access_code"
-        p_ [href_ "", class_ "text-sm underline underline-offset-1"] "date_of_birth"
-        p_ [href_ "", class_ "text-sm underline underline-offset-1"] "atm_passcodes"
-      div_ [class_ " mx-6 border-r border-yellow-400 p-5"] $ do
-        h5_ [class_ "text-black text-neutral-800 mb-4 -mt-8 text-base"] "Deleted Fields (2)"
-        p_ [href_ "", class_ "text-sm text-red-400 underline underline-offset-1"] "access_code"
-        p_ [href_ "", class_ "text-sm text-red-400 underline underline-offset-1"] "date_of_birth"
-      div_ [class_ " mx-6 p-5"] $ do
-        h5_ [class_ "text-black text-neutral-800 mb-4 -mt-8 text-base"] "Changed Field (1)"
-        div_ [class_ "flex" ]$ do
-          p_ [href_ "", class_ "text-sm underline underline-offset-1 mr-4"] "access_code"
-          img_ [src_ "/assets/svgs/cheveron-right.svg", class_ " mr-4"] 
-          p_ [href_ "", class_ "text-sm text-red-400 underline underline-offset-1 "] "access_code"
-      a_ [class_ " mx-4 mt-5 absolute m-5 bottom-0 right-0  text-sm cursor-pointer text-slate-600" ] "Resolve"
-    a_ [href_ ""] $ do 
-      div_ [class_ "flex flex-row justify-between bg-white mt-5 rounded-xl p-4"] $ do 
-        h2_ [class_ "text-slate-700 text-lg"] "Request fields"
-        img_ [src_ "/assets/svgs/darkplus.svg", class_ "h-4 mt-1 w-4"] 
+    div_ [class_ "flex justify-between mt-5"] $ do
+      div_ [class_ "flex flex-row"] $ do
+        img_ [src_ "/assets/svgs/cheveron-down.svg", class_ "h-4 mr-3 mt-1 w-4" ]
+        span_ [class_ "text-lg text-slate-700"] "Request"
+      div_ [class_ "flex flex-row mt-2"] $ do
+        img_ [src_ "/assets/svgs/leftarrow.svg", class_ " m-2" ]
+        span_ [src_ " mx-4"] "1/1"
+        img_ [src_ "/assets/svgs/rightarrow.svg", class_ " m-2" ]
+    div_ [class_ "bg-white rounded-xl p-5"] $ do 
+        div_ [class_ "flex flex-row "] $ do
+          img_ [src_ "/assets/svgs/cheveron-down.svg", class_ "h-4 mr-3 mt-4 w-4" ]
+          div_ [class_ "bg-gray-100 px-10 rounded-xl w-full p-4 text-sm text-slate-600"] "REQUEST HEADERS"
+        div_ [class_ "grid justify-items-stretch"] $ do
+          div_ [class_ "border flex mb-5 flex-row border-gray-100 justify-self-end px-5 p-3 rounded-xl mt-2 "] $ do
+            input_ [type_ "checkbox" ,class_ " mr-8 m-1" ] 
+            span_ [class_ "grow text-sm text-slate-600"] "Authorization"
+            span_ [class_ "text-sm text-slate-500 mx-12"] "[] obj"
+            img_ [src_ "/assets/svgs/alert-red.svg", class_ " mx-10 "]
+            img_ [src_ "/assets/svgs/dots-vertical.svg", class_ "mx-5"]
+
+        div_ [class_ "flex flex-row "] $ do
+          img_ [src_ "/assets/svgs/cheveron-down.svg", class_ "h-4 mr-3 mt-4 w-4" ]
+          div_ [class_ "bg-gray-100 px-10 rounded-xl w-full p-4 text-sm text-slate-600"] "REQUEST HEADERS"
+        div_ [class_ "grid justify-items-stretch "] $ do
+          div_ [class_ "border flex flex-row mb-5 border-gray-100 justify-self-end px-5 p-3 rounded-xl mt-2 "] $ do
+            input_ [type_ "checkbox" ,class_ " mr-8 m-1" ] 
+            span_ [class_ "grow text-sm text-slate-600"] "Authorization"
+            span_ [class_ "text-sm text-slate-500 mx-12"] "[] obj"
+            img_ [src_ "/assets/svgs/dots-vertical.svg", class_ "mx-5"]
+
     
