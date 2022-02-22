73e93fba4fc9e6a038d6a5f397195cd99b85fe17
DavidHODs <davidoluwatobi41@gmail.com>
2022-02-22 04:19:50 +0000

    removing invite member form validation till i figure out how to convert CI Text to Text

--- src/Pages/Projects/CreateProject.hs
+++ src/Pages/Projects/CreateProject.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Pages.Projects.CreateProject
   ( CreateProjectForm,
@@ -16,6 +17,7 @@ import Config
 import Control.Concurrent (forkIO)
 import Data.Default
 import qualified Data.Text as T
+import qualified Data.CaseInsensitive as CI
 import qualified Data.UUID as UUID
 import qualified Data.UUID.V4 as UUIDV4
 import Data.Valor (Valid, Valor, check1, failIf, validateM)
@@ -63,35 +65,28 @@ createProjectFormV =
     <$> check1 title (failIf ["name can't be empty"] T.null)
     <*> check1 description Valor.pass
 
-
 data InviteProjectMemberForm = InviteProjectMemberForm 
-  { memberEmail :: Text,
-    memberPriviledge :: Text
+  { email :: CI.CI Text,
+    permission :: ProjectMembers.Permissions
   }
   deriving (Eq, Show, Generic)
-  deriving anyclass (FromForm, Default)
 
 data InviteProjectMemberFormError = InviteProjectMemberFormError
-  { memberEmailE :: Maybe [String],
-    memberPriviledgeE :: Maybe [String]
+  { emailE :: Maybe [String],
+    permissionE :: Maybe [String]
   }
   deriving (Eq, Show, Generic)
   deriving anyclass (Default)
 
--- default permission level will be edit, changes can be made in settings
-inviteProjectMemberFormToModel :: Projects.ProjectId ->  Users.UserId -> InviteProjectMemberForm -> ProjectMembers.CreateProjectMembers
-inviteProjectMemberFormToModel pid uid InviteProjectMemberForm {..} = ProjectMembers.CreateProjectMembers {projectId = pid, userId = uid, ..}
+inviteProjectMember :: Projects.ProjectId ->  Users.UserId -> InviteProjectMemberForm -> ProjectMembers.CreateProjectMembers
+inviteProjectMember pid uid InviteProjectMemberForm { permission } = ProjectMembers.CreateProjectMembers { projectId, userId, permission }
+  where
+    projectId = pid
+    userId = uid
 
--- a package will be introduced if this does not work
-checkEmail :: Text -> Bool
-checkEmail = isJust . T.find (== '@')
-
-inviteProjectMemberFormV :: Monad m => Valor InviteProjectMemberForm m InviteProjectMemberFormError
-inviteProjectMemberFormV =
-  InviteProjectMemberFormError 
-    <$> check1 memberEmail (failIf ["email can't be blank"] T.null)
-    -- <*> check1 memberEmail (failIf ["must be a valid email address"] checkEmail)
-    <*> check1 memberPriviledge (failIf ["option can't be blank"] T.null)
+-- userid will be generated in Users.createUser
+createUserFromInvitation :: InviteProjectMemberForm -> Users.User
+createUserFromInvitation InviteProjectMemberForm { email } = Users.User { email }
 
 ----------------------------------------------------------------------------------------------------------
 -- createProjectGetH is the handler for the create projects page
