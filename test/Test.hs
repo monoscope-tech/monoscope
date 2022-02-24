be02cb82bbbcfc58e3697dea24de79b9644d83fa
Anthony Alaribe <anthonyalaribe@gmail.com>
2022-02-24 01:43:51 +0100

    add to tests

--- test/Test.hs
+++ test/Test.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Relude
-import qualified Spec
+import Spec qualified
 import System.Environment (getArgs)
 import Test.DocTest (mainFromCabal)
 import Test.Hspec.Formatters
@@ -9,5 +9,5 @@ import Test.Hspec.Runner
 
 main :: IO ()
 main = do
-  -- mainFromCabal "apitoolkit-server" =<< getArgs
+  mainFromCabal "apitoolkit-server" =<< getArgs
   hspecWith defaultConfig {configFormatter = Just progress} Spec.spec
