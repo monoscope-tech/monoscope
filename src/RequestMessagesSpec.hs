5041054b941b329e4725bf41b5050fac39ee3377
Anthony Alaribe <anthonyalaribe@gmail.com>
2022-08-15 21:42:21 +0200

    fix tests

--- src/RequestMessagesSpec.hs
+++ src/RequestMessagesSpec.hs
@@ -33,8 +33,8 @@ spec = do
       let expectedResp =
             [ (".menu.id", [AE.String "file"]),
               -- FIXME: We can correctly handle objects in arrays.
-              (".menu.popup.menuitem.[].value", [AE.String "v2", AE.String "v1"]),
-              (".menu.popup.menuitem.[].onclick", [AE.String "oc2", AE.String "oc1"])
+              (".menu.popup.menuitem.[].onclick", [AE.String "oc1", AE.String "oc2"]),
+              (".menu.popup.menuitem.[].value", [AE.String "v1", AE.String "v2"])
             ]
       RequestMessages.valueToFields exJSON `shouldBe` expectedResp
 
