efbef8d0de7839b0704fec3f3d66d9c56b90776c
dawkaka <yousiph77@gmail.com>
2023-05-14 11:18:21 +0000

    post swagger

--- src/Pages/Documentation.hs
+++ src/Pages/Documentation.hs
@@ -4,6 +4,7 @@ import Config
 import Data.Default (def)
 import Database.PostgreSQL.Entity.DBT (withPool)
 import Lucid
+import Lucid.Htmx
 import Models.Projects.Projects qualified as Projects
 import Models.Projects.RedactedFields qualified as RedactedFields
 import Models.Users.Sessions qualified as Sessions
@@ -29,11 +30,11 @@ documentationGetH sess pid = do
 
 documentationsPage :: Projects.ProjectId -> Html ()
 documentationsPage pid = do
-  div_ [class_ "container mx-auto relative  px-4 pt-10 pb-24 h-full"] $ do
+  div_ [class_ "container mx-auto relative  px-4 pt-10 pb-24 h-full", id_ "main-content"] $ do
     -- modal
     div_
-      [ style_ "background-color: rgba(0,0,0,0.5);z-index:99999",
-        class_ "fixed hidden items-center justify-center top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-full max-h-full",
+      [ style_ "z-index:99999",
+        class_ "fixed hidden pt-24 justify-center z-50 w-full p-4 bg-gray-500 bg-opacity-75 overflow-y-auto inset-0 h-full max-h-full",
         id_ "swaggerModal",
         tabindex_ "-1"
       ]
@@ -44,17 +45,22 @@ documentationsPage pid = do
           ]
           $ do
             -- Modal content
-            div_ [class_ "relative bg-white rounded-lg shadow"] $ do
-              div_ [class_ "flex items-start justify-between p-4 border-b rounded-t"] $ do
-                h3_ [class_ "text-xl font-semibold text-gray-900 dark:text-white"] "Upload Swagger"
-                button_ [type_ "button", class_ "btn bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center", onclick_ "closeModal()"] "Close"
-              -- Modal body
-              div_ [class_ "p-6 space-y-6"] $ do
-                textarea_ [style_ "height:65vh;resize:none", class_ "w-full border outline-none p-4 focus:outline-none focus:border-blue-200", placeholder_ "Paste swagger here"] ""
-              -- Modal footer
-              div_ [class_ "flex w-full justify-between items-center p-6 space-x-2 border-t border-gray-200 rounded-b"] $ do
-                button_ [type_ "button", class_ "btn", onclick_ "closeModal()"] "Close"
-                button_ [type_ "button", class_ "btn btn-primary"] "Upload"
+            form_
+              [ class_ "relative bg-white rounded-lg shadow",
+                hxPost_ $ "/p/" <> pid.toText <> "/swagger",
+                hxTarget_ "#main-content"
+              ]
+              $ do
+                div_ [class_ "flex items-start justify-between p-4 border-b rounded-t"] $ do
+                  h3_ [class_ "text-xl font-semibold text-gray-900 dark:text-white"] "Upload Swagger"
+                  button_ [type_ "button", class_ "btn bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center", onclick_ "closeModal()"] "Close"
+                -- Modal body
+                div_ [class_ "p-6 space-y-6"] $ do
+                  textarea_ [style_ "height:65vh;resize:none", class_ "w-full border outline-none p-4 focus:outline-none focus:border-blue-200", placeholder_ "Paste swagger here"] ""
+                -- Modal footer
+                div_ [class_ "flex w-full justify-between items-center p-6 space-x-2 border-t border-gray-200 rounded-b"] $ do
+                  button_ [type_ "button", class_ "btn", onclick_ "closeModal()"] "Close"
+                  button_ [type_ "sumbit", class_ "btn btn-primary"] "Upload"
 
     -- page content
     div_ [class_ "flex flex-col justify-between"] $ do
--- src/Pages/Endpoints/EndpointList.hs
+++ src/Pages/Endpoints/EndpointList.hs
@@ -12,6 +12,7 @@ import Lucid
 import Lucid.Htmx
 import Models.Apis.Endpoints qualified as Endpoints
 import Models.Projects.Projects qualified as Projects
+import Models.Projects.Projects qualified as Projets
 import Models.Users.Sessions qualified as Sessions
 import Pages.BodyWrapper (BWConfig (..), bodyWrapper)
 import Relude
@@ -31,15 +32,15 @@ endpointListH sess pid = do
             currProject = project,
             pageTitle = "Endpoints"
           }
-  pure $ bodyWrapper bwconf $ endpointList endpointStats
+  pure $ bodyWrapper bwconf $ endpointList endpointStats pid
 
-endpointList :: Vector Endpoints.EndpointRequestStats -> Html ()
-endpointList enps = do
+endpointList :: Vector Endpoints.EndpointRequestStats -> Projets.ProjectId -> Html ()
+endpointList enps pid = do
   div_ [class_ "container mx-auto relative  px-4 pt-10 pb-24 h-full overflow-y-scroll"] $ do
     -- modal
     div_
-      [ style_ "background-color: rgba(0,0,0,0.5);z-index:99999",
-        class_ "fixed hidden items-center justify-center top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-full max-h-full",
+      [ style_ "z-index:99999",
+        class_ "fixed hidden pt-24 justify-center z-50 w-full p-4 bg-gray-500 bg-opacity-75 overflow-y-auto inset-0 h-full max-h-full",
         id_ "swaggerModal",
         tabindex_ "-1"
       ]
@@ -50,17 +51,21 @@ endpointList enps = do
           ]
           $ do
             -- Modal content
-            div_ [class_ "relative bg-white rounded-lg shadow"] $ do
-              div_ [class_ "flex items-start justify-between p-4 border-b rounded-t"] $ do
-                h3_ [class_ "text-xl font-semibold text-gray-900 dark:text-white"] "Upload Swagger"
-                button_ [type_ "button", class_ "btn bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center", onclick_ "closeModal()"] "Close"
-              -- Modal body
-              div_ [class_ "p-6 space-y-6"] $ do
-                textarea_ [style_ "height:65vh;resize:none", class_ "w-full border outline-none p-4 focus:outline-none focus:border-blue-200", placeholder_ "Paste swagger here"] ""
-              -- Modal footer
-              div_ [class_ "flex w-full justify-between items-center p-6 space-x-2 border-t border-gray-200 rounded-b"] $ do
-                button_ [type_ "button", class_ "btn", onclick_ "closeModal()"] "Close"
-                button_ [type_ "button", class_ "btn btn-primary"] "Upload"
+            form_
+              [ class_ "relative bg-white rounded-lg shadow",
+                hxPost_ $ "/p/" <> pid.toText <> "/swagger"
+              ]
+              $ do
+                div_ [class_ "flex items-start justify-between p-4 border-b rounded-t"] $ do
+                  h3_ [class_ "text-xl font-semibold text-gray-900 dark:text-white"] "Upload Swagger"
+                  button_ [type_ "button", class_ "btn bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center", onclick_ "closeModal()"] "Close"
+                -- Modal body
+                div_ [class_ "p-6 space-y-6"] $ do
+                  textarea_ [style_ "height:65vh;resize:none", class_ "w-full border outline-none p-4 focus:outline-none focus:border-blue-200", placeholder_ "Paste swagger here"] ""
+                -- Modal footer
+                div_ [class_ "flex w-full justify-between items-center p-6 space-x-2 border-t border-gray-200 rounded-b"] $ do
+                  button_ [type_ "button", class_ "btn", onclick_ "closeModal()"] "Close"
+                  button_ [type_ "button", class_ "btn btn-primary"] "Upload"
 
     -- page content
     div_ [class_ "flex flex-col justify-between"] $ do
