770ddae9b2d3f199eb04713ade4620001288622f
dawkaka <yousiph77@gmail.com>
2023-09-22 09:25:40 +0000

    add links to dashobard stats

--- src/Pages/Components.hs
+++ src/Pages/Components.hs
@@ -3,16 +3,19 @@ module Pages.Components (statBox) where
 import Data.Text qualified as T
 import Fmt
 import Lucid
+import Models.Projects.Projects (ProjectId)
 import Relude
 import Utils
 
-statBox :: Text -> Text -> Int -> Maybe Int -> Html ()
-statBox title helpInfo val bckupValM = do
+statBox :: Maybe ProjectId -> Text -> Text -> Int -> Maybe Int -> Html ()
+statBox pid title helpInfo val bckupValM = do
   let tl = getTargetPage title
-  traceShowM tl
+  let pidT = case pid of
+        Just p -> p.toText
+        Nothing -> ""
   if not (T.null tl)
     then do
-      a_ [href_ tl, class_ "col-span-1 card-round p-5 flex flex-row content-between justify-between"] $ do
+      a_ [href_ $ "/p/" <> pidT <> tl, class_ "col-span-1 card-round p-5 flex flex-row content-between justify-between"] $ do
         div_ $ do
           div_ [class_ "inline-block flex flex-row content-between"] $ do
             strong_ [class_ "font-bold text-2xl"] $ toHtml @Text $ fmt (commaizeF val)
@@ -29,7 +32,7 @@ statBox title helpInfo val bckupValM = do
         span_ [class_ "inline-block", term "data-tippy-content" helpInfo] $ mIcon_ "info" "w-4 h-4"
 
 getTargetPage :: Text -> Text
-getTargetPage "Requests" = "log_explorer"
-getTargetPage "Anomalies" = "anomalies"
-getTargetPage "Endpoints" = "endpoints"
+getTargetPage "Requests" = "/log_explorer"
+getTargetPage "Anomalies" = "/anomalies"
+getTargetPage "Endpoints" = "/endpoints"
 getTargetPage _ = ""
