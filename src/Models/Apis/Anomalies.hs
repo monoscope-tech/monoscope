75d01e6b8ede30a9319046ddb9984ea3356cf677
dawkaka <yousiph77@gmail.com>
2023-09-26 08:06:22 +0000

    anomalies query to normal

--- src/Models/Apis/Anomalies.hs
+++ src/Models/Apis/Anomalies.hs
@@ -223,7 +223,7 @@ SELECT avm.id, avm.created_at, avm.updated_at, avm.project_id, aan.acknowleged_a
         AND avm.anomaly_type != 'field'
         AND rd.created_at > NOW() - interval '14 days' 
     GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
-    ORDER BY last_seen desc
+    ORDER BY $orderBy
     $skip
     $limit;
       |]
