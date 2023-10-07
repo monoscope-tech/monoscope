a2937dd63c96926438dcb06bbfe30d59cef05226
Anthony Alaribe <anthonyalaribe@gmail.com>
2023-10-08 00:03:47 +0200

    show hit in navbar

--- src/Pages/BodyWrapper.hs
+++ src/Pages/BodyWrapper.hs
@@ -301,9 +301,10 @@ sideNav sess project pageTitle menuItem hasIntegrated = do
         let isActive = maybe (pageTitle == mTitle) (== mTitle) menuItem
         let activeCls = if isActive then " bg-blue-50 text-blue-700 border-l-4 border-blue-700" else " text-slate-900"
         let intG = fromMaybe True hasIntegrated
-        let intGCls = if intG || (mTitle == "Get started") then " hover:bg-blue-50" else " cursor-not-allowed pointer-events-none "
+        let intGCls = if intG || (mTitle == "Get started") then " hover:bg-blue-50" else " cursor-not-allowed  "
         a_
-          [ href_ mUrl
+          [ href_ $ if isActive then mUrl else ""
+          , [__| on click halt|]
           , term "data-tippy-placement" "right"
           , term "data-tippy-content" (if intG || (mTitle == "Get started") then mTitle else "Our menus are shy. Help them come out by integrating the SDK.")
           , class_ $ "block flex gap-3 px-5 py-3 flex justify-center items-center " <> activeCls <> intGCls
