6ab2b0c41fc1b19e4ec9e7a0e813fa442b604eb3
dawkaka <yousiph77@gmail.com>
2023-10-09 21:37:34 +0000

    format with formoluo

--- src/Pages/Outgoing.hs
+++ src/Pages/Outgoing.hs
@@ -1,3 +1,4 @@
+
 module Pages.Outgoing (outgoingGetH) where
 
 import Config
@@ -18,36 +19,37 @@ import Utils
 
 outgoingGetH :: Sessions.PersistentSession -> Projects.ProjectId -> DashboardM (Html ())
 outgoingGetH sess pid = do
-    pool <- asks pool
-    isMember <- liftIO $ withPool pool $ userIsProjectMember sess pid
-    if not isMember
-        then do
-            pure $ userNotMemeberPage sess
-        else do
-            (project, hostsEvents) <- liftIO $ withPool pool do
-                project <- Projects.projectById pid
-                hostsAndEvents <- RequestDump.dependenciesAndEventsCount pid
-                pure (project, hostsAndEvents)
-            let bwconf =
-                    (def :: BWConfig)
-                        { sessM = Just sess
-                        , currProject = project
-                        , pageTitle = "Dependencies"
-                        }
-            pure $ bodyWrapper bwconf $ outgoingPage pid hostsEvents
+  pool <- asks pool
+  isMember <- liftIO $ withPool pool $ userIsProjectMember sess pid
+  if not isMember
+    then do
+      pure $ userNotMemeberPage sess
+    else do
+      (project, hostsEvents) <- liftIO $ withPool pool do
+        project <- Projects.projectById pid
+        hostsAndEvents <- RequestDump.dependenciesAndEventsCount pid
+        pure (project, hostsAndEvents)
+      let bwconf =
+            (def :: BWConfig)
+              { sessM = Just sess
+              , currProject = project
+              , pageTitle = "Dependencies"
+              }
+      pure $ bodyWrapper bwconf $ outgoingPage pid hostsEvents
 
 
 outgoingPage :: Projects.ProjectId -> V.Vector RequestDump.HostEvents -> Html ()
 outgoingPage pid hostsEvents = div_ [class_ "w-full mx-auto px-16 pt-10 pb-24"] $ do
-    h3_ [class_ "text-xl text-slate-700 flex place-items-center"] "Dependencies"
-    div_ [class_ "mt-8 space-y-4 py-8 "] do
-        div_ [class_ "flex px-8 w-full justify-between"] do
-            h3_ [class_ "font-bold"] "Hosts"
-            h3_ [class_ "font-bold"] "Events"
-        div_ [class_ "flex flex-col"] do
-            forM_ hostsEvents $ \host -> do
-                a_ [href_ $ "/p/" <> pid.toText <> "/endpoints?host=" <> host.host, class_ "flex p-8 border border-t-transparent justify-between items-center hover:bg-gray-50"] $ do
-                    span_ [class_ "p-2", href_ $ "/p/" <> pid.toText <> "/endpoints?host=" <> host.host] $ toHtml host.host
-                    -- div_ [class_ "w-[200px] h-[80px] mt-4 shrink-0"] pass
-                    -- Charts.throughput pid (host.host) (Just $ Charts.QBHost host.host) (Just Charts.GBHost) 14 Nothing False (Nothing, Nothing) Nothing
-                    div_ [] $ show host.eventCount
+  h3_ [class_ "text-xl text-slate-700 flex place-items-center"] "Dependencies"
+  div_ [class_ "mt-8 space-y-4 py-8 "] do
+    div_ [class_ "flex px-8 w-full justify-between"] do
+      h3_ [class_ "font-bold"] "Hosts"
+      h3_ [class_ "font-bold"] "Events"
+    div_ [class_ "flex flex-col"] do
+      forM_ hostsEvents $ \host -> do
+        a_ [href_ $ "/p/" <> pid.toText <> "/endpoints?host=" <> host.host, class_ "flex p-8 border border-t-transparent justify-between items-center hover:bg-gray-50"] $ do
+          span_ [class_ "p-2", href_ $ "/p/" <> pid.toText <> "/endpoints?host=" <> host.host] $ toHtml host.host
+          -- div_ [class_ "w-[200px] h-[80px] mt-4 shrink-0"] pass
+          -- Charts.throughput pid (host.host) (Just $ Charts.QBHost host.host) (Just Charts.GBHost) 14 Nothing False (Nothing, Nothing) Nothing
+          div_ [] $ show host.eventCount
+          
