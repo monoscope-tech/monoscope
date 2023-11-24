a18b3cd4b5c86399318f9b16aedae2f874baaf82
dawkaka <yousiph77@gmail.com>
2023-11-24 22:08:51 +0000

    feat: dependency list touches

--- src/Pages/Outgoing.hs
+++ src/Pages/Outgoing.hs
@@ -2,6 +2,7 @@ module Pages.Outgoing (outgoingGetH) where
 
 import Config
 import Data.Default (def)
+import Data.Text qualified as T
 import Data.Vector qualified as V
 import Database.PostgreSQL.Entity.DBT
 import Lucid
@@ -39,20 +40,26 @@ outgoingGetH sess pid = do
 
 outgoingPage :: Projects.ProjectId -> V.Vector Endpoints.HostEvents -> Html ()
 outgoingPage pid hostsEvents = div_ [class_ "w-full mx-auto px-16 pt-10 pb-24"] $ do
-  h3_ [class_ "text-xl text-slate-700 flex place-items-center"] "Dependencies"
-  div_ [class_ "mt-8 space-y-4 py-8 "] do
+  h3_ [class_ "text-xl text-slate-700 flex place-items-center"] "Outbound Integrations"
+  div_ [class_ "mt-8 mx-auto space-y-4 max-w-[1000px]"] do
     div_ [class_ "flex px-8 w-full justify-between"] do
-      h3_ [class_ "font-bold"] "Hosts"
+      h3_ [class_ "font-bold"] "Host"
       h3_ [class_ "font-bold"] "Events"
     div_ [class_ "flex flex-col"] do
       forM_ hostsEvents $ \host -> do
         div_ [class_ "flex border border-t-transparent items-center"] do
-          a_ [href_ $ "/p/" <> pid.toText <> "/endpoints?host=" <> host.host, class_ "flex  w-full justify-between items-center p-8 hover:bg-gray-50"] $ do
-            span_ [class_ "p-2", href_ $ "/p/" <> pid.toText <> "/endpoints?host=" <> host.host] $ toHtml host.host
+          a_
+            [ href_ $ "/p/" <> pid.toText <> "/log_explorer?query=host%20%3D%20" <> "\"" <> host.host <> "\""
+            , class_ "p-2 shrink-0 pl-8 flex items-center gap-1 text-blue-500 hover:text-slate-600"
+            ]
+            do
+              "Logs"
+              faIcon_ "fa-arrow-up-right" "fa-solid fa-arrow-up-right" "h-3 w-3"
+          a_ [href_ $ "/p/" <> pid.toText <> "/endpoints?host=" <> host.host, class_ "flex  w-full justify-between items-center p-8"] $ do
+            span_ [class_ "p-2", href_ $ "/p/" <> pid.toText <> "/endpoints?host=" <> host.host] $ toHtml (T.replace "http://" "" $ T.replace "https://" "" host.host)
           -- div_ [class_ "w-[200px] h-[80px] mt-4 shrink-0"] do
           -- Charts.throughput pid host.host (Just (QBHost host.host)) Nothing 14 Nothing False (Nothing, Nothing) Nothing
           div_ [class_ "shrink-0 flex items-center gap-10 p-8"] do
-            a_ [href_ $ "/p/" <> pid.toText <> "/log_explorer?query=host%20%3D%20" <> "\"" <> host.host <> "\"", class_ "p-2 shrink-0 pl-8 text-blue-500 hover:text-slate-600"] "View logs"
             span_ [] $ show host.eventCount
     when (null hostsEvents) $ div_ [class_ "flex flex-col text-center justify-center items-center h-32"] $ do
       strong_ "No dependencies yet."
