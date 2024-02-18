ed10d98fa44286fa5985000f299f204f7e842a07
dawkaka <yousiph77@gmail.com>
2024-02-18 20:14:47 +0000

    test resolve refs

--- static/public/assets/js/parse_swagger.test.js
+++ static/public/assets/js/parse_swagger.test.js
@@ -1,8 +1,76 @@
-const {resolveRefs} = require("./parse_swagger")
-
+const { resolveRefs } = require("./parse_swagger.js").default;
 
 it("Should resolve all refs", () => {
-  
-    
+  const components = {
+    parameters: {
+      offsetParam: {
+        in: "query",
+        schema: {
+          type: "integer",
+          minimum: 0,
+        },
+        description:
+          "The number of items to skip before starting to collect the result set.",
+      },
+      limitParam: {
+        in: "query",
+        name: "limit",
+        required: false,
+        schema: {
+          type: "integer",
+          default: 20,
+        },
+      },
+    },
+  };
 
-})
+  const data = {
+    get: {
+      summary: "Gets a list of users.",
+      parameters: [
+        {
+          $ref: "#/components/parameters/offsetParam",
+        },
+        {
+          $ref: "#/components/parameters/limitParam",
+        },
+      ],
+      responses: {
+        200: {
+          description: "OK",
+        },
+      },
+    },
+  };
+  const expectedData = {
+    get: {
+      summary: "Gets a list of users.",
+      parameters: [
+        {
+          in: "query",
+          schema: {
+            type: "integer",
+            minimum: 0,
+          },
+          description:
+            "The number of items to skip before starting to collect the result set.",
+        },
+        {
+          in: "query",
+          name: "limit",
+          required: false,
+          schema: {
+            type: "integer",
+            default: 20,
+          },
+        },
+      ],
+      responses: {
+        200: {
+          description: "OK",
+        },
+      },
+    },
+  };
+  expect(resolveRefs(data, components)).toMatchObject(expectedData);
+});
