e337fb3659070fef4ba94ffb94eddb4680571eac
dawkaka <yousiph77@gmail.com>
2024-02-19 08:39:56 +0000

    test query params

--- static/public/assets/js/parse_swagger.test.js
+++ static/public/assets/js/parse_swagger.test.js
@@ -1,10 +1,12 @@
-const { resolveRefs } = require("./parse_swagger.js").default;
+const { resolveRefs, parseHeadersAndParams } =
+  require("./parse_swagger.js").default;
 
 it("Should resolve all refs", () => {
   const components = {
     parameters: {
       offsetParam: {
         in: "query",
+        name: "offset",
         schema: {
           type: "integer",
           minimum: 0,
@@ -18,6 +20,7 @@ it("Should resolve all refs", () => {
         required: false,
         schema: {
           type: "integer",
+          example: 20,
           default: 20,
         },
       },
@@ -35,11 +38,6 @@ it("Should resolve all refs", () => {
           $ref: "#/components/parameters/limitParam",
         },
       ],
-      responses: {
-        200: {
-          description: "OK",
-        },
-      },
     },
   };
   const expectedData = {
@@ -48,6 +46,7 @@ it("Should resolve all refs", () => {
       parameters: [
         {
           in: "query",
+          name: "offset",
           schema: {
             type: "integer",
             minimum: 0,
@@ -65,9 +64,114 @@ it("Should resolve all refs", () => {
           },
         },
       ],
-      responses: {
-        200: {
-          description: "OK",
+    },
+  };
+
+  const expectedQueryParamsData = [
+    {
+      keypath: ".offset",
+      type: "number",
+      format: "int64",
+      example: "",
+      description:
+        "The number of items to skip before starting to collect the result set.",
+    },
+    {
+      keypath: ".limit",
+      type: "number",
+      example: 20,
+      format: "int64",
+      description: "",
+    },
+  ];
+  expect(resolveRefs(data, components)).toMatchObject(expectedData);
+  expect(
+    parseHeadersAndParams({}, data.get.parameters, components)
+      .queryParamsKeyPaths
+  ).toMatchObject(expectedQueryParamsData);
+});
+
+it("should recursively resolve all refs", () => {
+  const data = {
+    paths: {
+      "/users": {
+        get: {
+          summary: "Retrieves a list of users.",
+          responses: {
+            200: {
+              description: "A list of users.",
+              content: {
+                "application/json": {
+                  schema: {
+                    $ref: "#/components/schemas/UserList",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+  const components = {
+    schemas: {
+      UserList: {
+        type: "object",
+        properties: {
+          users: {
+            type: "array",
+            items: {
+              $ref: "#/components/schemas/User",
+            },
+          },
+        },
+      },
+      User: {
+        name: { type: "string", format: "text", example: "Jon Doe" },
+        age: {
+          type: "number",
+          format: "int64",
+          example: 25,
+          description: "the users age",
+        },
+      },
+    },
+  };
+  const expectedData = {
+    paths: {
+      "/users": {
+        get: {
+          summary: "Retrieves a list of users.",
+          responses: {
+            200: {
+              description: "A list of users.",
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "object",
+                    properties: {
+                      users: {
+                        type: "array",
+                        items: {
+                          name: {
+                            type: "string",
+                            format: "text",
+                            example: "Jon Doe",
+                          },
+                          age: {
+                            type: "number",
+                            format: "int64",
+                            example: 25,
+                            description: "the users age",
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
         },
       },
     },
