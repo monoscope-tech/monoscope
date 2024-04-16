d2ab31b0a367724a6d207ef2ecc8cd044b1121e6
Yussif Mohammed <yousiph77@gmail.com>
2024-04-16 12:38:56 +0000

    Update BodyWrapper.hs
    Google tag

--- src/Pages/BodyWrapper.hs
+++ src/Pages/BodyWrapper.hs
@@ -219,6 +219,17 @@ fbq('init', '3674513372787915');
 fbq('track', 'PageView');
       |]
 
+
+      -- Facebook Pixel Code --
+      script_
+        [text|
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TF4BQQ3D');
+      |]
+
     noscript_ [] do
       img_ [height_ "1", width_ "1", src_ "https://www.facebook.com/tr?id=3674513372787915&ev=PageView&noscript=1"]
     -- End Facebook Pixel Code
