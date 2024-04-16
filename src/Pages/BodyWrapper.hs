c288071e9c18f231362d5a33a45bd3dc20fd7d2f
Yussif Mohammed <yousiph77@gmail.com>
2024-04-16 10:30:24 +0000

    Update BodyWrapper.hs

--- src/Pages/BodyWrapper.hs
+++ src/Pages/BodyWrapper.hs
@@ -206,6 +206,23 @@ s.parentNode.insertBefore(t,s)}(window,document,'script',
  fbq('init', '1135987380886994'); 
 fbq('track', 'PageView');
       |]
+
+      -- Facebook Pixel Code --
+      script_
+        [text|
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '3674513372787915');
+fbq('track', 'PageView');
+      |]
+
+    noscript_ [] do
+      img_ [height_ "1", width_ "1", src_ "https://www.facebook.com/tr?id=3674513372787915&ev=PageView&noscript=1"]
+    -- End Facebook Pixel Code
+
     noscript_ [] do
       img_ [height_ "1", width_ "1", src_ "https://www.facebook.com/tr?id=1135987380886994&ev=PageView&noscript=1"]
     -- End Facebook Pixel Code
