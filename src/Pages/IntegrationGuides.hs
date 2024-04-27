eb74a6e0ae7815e2d06eb23d6ca0cdee604ea7b0
dawkaka <yousiph77@gmail.com>
2024-04-27 09:18:58 +0000

    format

--- src/Pages/IntegrationGuides.hs
+++ src/Pages/IntegrationGuides.hs
@@ -12,10 +12,10 @@ import Models.Projects.ProjectApiKeys qualified as ProjectApiKeys
 import Models.Projects.Projects qualified as Projects
 import Models.Users.Sessions qualified as Sessions
 import NeatInterpolation (text)
-import Pages.BodyWrapper
-  ( BWConfig (currProject, pageTitle, sessM),
-    bodyWrapper,
-  )
+import Pages.BodyWrapper (
+  BWConfig (currProject, pageTitle, sessM),
+  bodyWrapper,
+ )
 import Pages.IntegrationDemos.DotNet
 import Pages.IntegrationDemos.ExpressJs
 import Pages.IntegrationDemos.Gin
@@ -25,6 +25,7 @@ import Relude.Unsafe qualified as Unsafe
 import System.Config (AuthContext)
 import System.Types (ATAuthCtx)
 import Utils
+
 
 getH :: Projects.ProjectId -> Maybe Text -> Maybe Text -> Maybe Text -> ATAuthCtx (Html ())
 getH pid sdkM errReportM reqMonM = do
@@ -38,11 +39,12 @@ getH pid sdkM errReportM reqMonM = do
     pure (key, project)
   let bwconf =
         (def :: BWConfig)
-          { sessM = Just sess,
-            currProject = project,
-            pageTitle = "Integrations"
+          { sessM = Just sess
+          , currProject = project
+          , pageTitle = "Integrations"
           }
   pure $ bodyWrapper bwconf $ integrationsPage pid (fromMaybe "express" sdkM) apiKey errReportM reqMonM
+
 
 integrationsPage :: Projects.ProjectId -> Text -> Text -> Maybe Text -> Maybe Text -> Html ()
 integrationsPage pid sdk apiKey errReportM reqMonM = do
@@ -65,18 +67,18 @@ integrationsPage pid sdk apiKey errReportM reqMonM = do
             a_ [class_ "px-2 py-1 hover:bg-gray-200", href_ $ baseUrl <> "sdk=pyramid"] "Python Pyramid"
             a_ [class_ "px-2 py-1 hover:bg-gray-200", href_ $ baseUrl <> "sdk=dotnet"] ".NET"
         button_
-          [ class_ "rounded-lg flex items-center gap-2 border px-4 py-1.5 font-medium text-sm hover:bg-gray-100",
-            [__|on click go to the top of #requests-monitoring|]
+          [ class_ "rounded-lg flex items-center gap-2 border px-4 py-1.5 font-medium text-sm hover:bg-gray-100"
+          , [__|on click go to the top of #requests-monitoring|]
           ]
           "Request monitoring"
         button_
-          [ class_ "rounded-lg flex items-center gap-2 border px-4 py-1.5 font-medium text-sm hover:bg-gray-100",
-            [__|on click go to the top of #errors-monitoring|]
+          [ class_ "rounded-lg flex items-center gap-2 border px-4 py-1.5 font-medium text-sm hover:bg-gray-100"
+          , [__|on click go to the top of #errors-monitoring|]
           ]
           "Error Reporting"
         button_
-          [ class_ "rounded-lg flex items-center gap-2 border px-4 py-1.5 font-medium text-sm hover:bg-gray-100",
-            [__|on click go to the top of #outgoing-request-monitoring|]
+          [ class_ "rounded-lg flex items-center gap-2 border px-4 py-1.5 font-medium text-sm hover:bg-gray-100"
+          , [__|on click go to the top of #outgoing-request-monitoring|]
           ]
           "Outgoing request monitoring"
     div_ [class_ "px-8 mb-10"] do
@@ -89,6 +91,7 @@ integrationsPage pid sdk apiKey errReportM reqMonM = do
       [text|
 hljs.highlightAll();
  |]
+
 
 getTitle :: Text -> Text
 getTitle "gin" = "GO Gin"
