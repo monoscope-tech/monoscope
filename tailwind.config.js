2c45b5927058df32c07f813b96ca5fe1221d659c
dawkaka <yousiph77@gmail.com>
2024-05-10 14:10:24 +0000

    fix log explore query empty state and filtercomponent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,hs}", "./static/public/assets/testeditor.js", "./static/public/assets/steps-editor.js"],
+  content: ['./src/**/*.{html,js,hs}', './static/public/assets/testeditor.js', './static/public/assets/steps-editor.js', './static/public/assets/filtercomponent.js'],
   daisyui: {
-    themes: ["light", "dark", "winter", "dim"],
+    themes: ['light', 'dark', 'winter', 'dim'],
   },
   theme: {
     extend: {
       fontFamily: {
-        Poppins: ["Poppins", "sans-serif"],
-        Inconsolata: ["Inconsolata", "monospace"],
+        Poppins: ['Poppins', 'sans-serif'],
+        Inconsolata: ['Inconsolata', 'monospace'],
       },
     },
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
-};
+  plugins: [require('daisyui'), require('@tailwindcss/typography')],
+}
