706de0f4b92cfc9f3a9bca3d0a3913760766d23d
jesseamos <anietiejesse@gmail.com>
2024-05-31 11:30:35 +0100

    still working on the tabs functionality in the testing page

--- src/Models/Tests/Testing.hs
+++ src/Models/Tests/Testing.hs
@@ -17,6 +17,8 @@ module Models.Tests.Testing (
   getCollections,
   getCollectionById,
   getCollectionsId,
+  TabStatus(..)
+  
 )
 where
 
@@ -191,6 +193,7 @@ data CollectionListItem = ReportListItem
   , description :: Text
   , stepsCount :: Int
   , schedule :: Text
+  , isScheduled :: Bool
   }
   deriving stock (Show, Generic)
   deriving anyclass (FromRow, ToRow, NFData)
@@ -234,6 +237,8 @@ data StepResult = StepResult
   deriving (AE.FromJSON, AE.ToJSON) via DAE.CustomJSON '[DAE.FieldLabelModifier '[DAE.CamelToSnake]] StepResult
 
 
+data TabStatus = Active | Inactive | Deactive
+
 addCollection :: Collection -> DBT IO ()
 addCollection = insert @Collection
 
@@ -259,23 +264,28 @@ getCollectionById id' = queryOne Select q (Only id')
 
 
 -- TODO: delete or remove the collect_steps join
-getCollections :: Projects.ProjectId -> DBT IO (V.Vector CollectionListItem)
-getCollections pid = query Select q (Only pid)
+getCollections :: Projects.ProjectId -> TabStatus -> DBT IO (V.Vector CollectionListItem)
+getCollections pid tabStatus = query Select q (Only pid)
   where
-    q =
-      [sql| SELECT t.id id , t.created_at created_at , t.updated_at updated_at, t.project_id project_id, t.last_run last_run, 
-                  t.title title, t.description description, 0,CASE
-                      WHEN EXTRACT(DAY FROM t.schedule) > 0 THEN CONCAT(EXTRACT(DAY FROM t.schedule)::TEXT, ' days')
-                      WHEN EXTRACT(HOUR FROM t.schedule) > 0 THEN CONCAT(EXTRACT(HOUR FROM t.schedule)::TEXT, ' hours')
-                      ELSE CONCAT(EXTRACT(MINUTE FROM t.schedule)::TEXT, ' minutes')
-                  END as  schedule
-                  FROM  tests.collections t
-                  WHERE t.project_id = ?
-                  GROUP BY t.id
-                  ORDER BY t.updated_at DESC;
-  |]
+    statusCondition = case tabStatus of
+      Active   -> "is_scheduled = TRUE"
+      Inactive -> "is_scheduled = FALSE"
+      Deactive -> "is_scheduled = FALSE"
 
-
+    q = [sql|
+      SELECT t.id, t.created_at, t.updated_at, t.project_id, t.last_run, 
+             t.title, t.description, 0, 
+             CASE
+               WHEN EXTRACT(DAY FROM t.schedule) > 0 THEN CONCAT(EXTRACT(DAY FROM t.schedule)::TEXT, ' days')
+               WHEN EXTRACT(HOUR FROM t.schedule) > 0 THEN CONCAT(EXTRACT(HOUR FROM t.schedule)::TEXT, ' hours')
+               ELSE CONCAT(EXTRACT(MINUTE FROM t.schedule)::TEXT, ' minutes')
+             END as schedule,
+             t.is_scheduled
+      FROM tests.collections t
+      WHERE t.project_id = ? AND |] <> statusCondition <> [sql|
+      GROUP BY t.id
+      ORDER BY t.updated_at DESC;
+    |]
 getCollectionsId :: DBT IO (V.Vector CollectionId)
 getCollectionsId = query Select q ()
   where
--- src/Pages/Monitors/Testing.hs
+++ src/Pages/Monitors/Testing.hs
@@ -1,3 +1,4 @@
+
 module Pages.Monitors.Testing (
   testingGetH,
   testingPostH,
@@ -30,14 +31,14 @@ import Lucid (
   label_,
   name_,
   onclick_,
-  p_,
   placeholder_,
   script_,
   span_,
   textarea_,
   type_,
+  small_
  )
-import Lucid.Htmx (hxPost_, hxSwap_, hxTarget_)
+import Lucid.Htmx (hxPost_, hxSwap_, hxTarget_,hxBoost_)
 import Lucid.Hyperscript (__)
 import Models.Projects.Projects qualified as Projects
 import Models.Tests.Testing qualified as Testing
@@ -67,7 +68,7 @@ data ScheduleForm = ScheduleForm
   deriving anyclass (FromForm, AE.FromJSON)
 
 
-testingPostH :: Projects.ProjectId -> TestCollectionForm -> ATAuthCtx (RespHeaders (Html ()))
+testingPostH :: Projects.ProjectId ->  TestCollectionForm -> ATAuthCtx (RespHeaders (Html ()))
 testingPostH pid collection = do
   (_, project) <- Sessions.sessionAndProject pid
   if collection.collection_id == ""
@@ -89,20 +90,25 @@ testingPostH pid collection = do
               , collectionSteps = Testing.CollectionSteps V.empty
               }
       _ <- dbtToEff $ Testing.addCollection coll
-      cols <- dbtToEff $ Testing.getCollections pid
+      cols <- dbtToEff $ Testing.getCollections pid Testing.Active
       addSuccessToast "Collection added Successfully" Nothing
       addRespHeaders $ testingPage pid cols
     else do
       -- _ <- dbtToEff $ Testing.updateCollection pid collection.collection_id collection.title collection.description
-      cols <- dbtToEff $ Testing.getCollections pid
+      cols <- dbtToEff $ Testing.getCollections pid Testing.Active
       addSuccessToast "Collection updated Successfully" Nothing
       addRespHeaders $ testingPage pid cols
 
 
-testingGetH :: Projects.ProjectId -> ATAuthCtx (RespHeaders (Html ()))
-testingGetH pid = do
+testingGetH :: Projects.ProjectId -> Maybe Text ->  ATAuthCtx (RespHeaders (Html ()))
+testingGetH pid maybeTab= do
+  let tabStatus = case maybeTab of
+        Just "Active"   -> Testing.Active
+        Just "Inactive" -> Testing.Inactive
+        Just "Deactive" -> Testing.Deactive
+        _               -> Testing.Active  -- Default to Active if tab is unrecognized
   (sess, project) <- Sessions.sessionAndProject pid
-  colls <- dbtToEff $ Testing.getCollections pid
+  colls <- dbtToEff $ Testing.getCollections pid tabStatus
   let bwconf =
         (def :: BWConfig)
           { sessM = Just sess.persistentSession
@@ -118,48 +124,80 @@ testingPage pid colls = do
     modal pid
     div_ [class_ "w-full mt-4 max-w-7xl mx-auto"] do
       div_ [class_ "flex justify-between border-b py-2 items-center"] do
-        h1_ [class_ "text-3xl font-bold"] "Test Collections"
+        h1_ [class_ "text-xl text-slate-700 flex place-items-center"] "Test Collections"
         button_
-          [ class_ "text-white rounded bg-blue-500 px-4 py-2 flex items-center gap-2"
+          [ class_ "w-max btn btn-indigo -ml-1 text-md"
           , [__|on click remove .hidden from #col-modal then set #collection_id's value to ""|]
           ]
           $ (faSprite_ "plus" "regular" "h-6 w-6" >> "Collection")
-      div_ [class_ "w-full grid grid-cols-2 gap-8 mt-8"] $ forM_ colls \c -> collectionCard pid c
+    div_ [class_ "w-full mx-auto px-16 pt-5 pb-24 overflow-y-scroll h-full"] $ do     
+      div_ [class_ "py-2 px-2 space-x-6 border-b border-slate-20 mt-6 mb-8 text-sm font-light", hxBoost_ "true"] do
+        a_ [class_"inline-block py-2 ",href_ ("/p/" <> pid.toText <> "/testing/?tab=Active")] "Active"
+        a_ [class_"inline-block  py-2 ",href_ ("/p/" <> pid.toText <> "/testing/?tab=Inactive")] "InActive"
+        a_ [class_"inline-block  py-2 ",href_ ("/p/" <> pid.toText <> "/testing/?tab=Deactive")] "Deactive"
+      div_ [class_"w-full card-round"] $ collectionCardList pid colls
+      
+collectionCardList :: Projects.ProjectId -> V.Vector Testing.CollectionListItem -> Html ()
+collectionCardList pid colls = do 
+ form_ [class_ "col-span-5 bg-white divide-y "] $ do
+    div_ [class_ "flex py-3 gap-8 items-center  bg-gray-50"] do
+      div_ [class_ "h-4 flex space-x-3 w-8"] do
+        a_ [class_ " w-2 h-full"] ""
+        input_ [term "aria-label" "Select Issue", type_ "checkbox"]
+      div_ [class_ " grow flex flex-row gap-2"] do
+        button_ [class_ "btn btn-sm btn-outline border-black hover:shadow-2xl"] "✓ acknowlege"
+      div_ [class_ "flex justify-center font-base w-60 content-between gap-14"] do
+        span_ "Created at"
+        div_ [class_ " space-x-2 font-base text-sm"] $ do
+          a_ [class_ "cursor-pointer"] "24h"
+          a_ [class_ "cursor-pointer font-bold text-base"] "14d"
+      div_ [class_ "w-36 flex items-center justify-center"] $ span_ [class_ "font-base"] "Last modified"
+      div_ [class_ "w-36 flex items-center justify-center"] $ span_ [class_ "font-base"] "Last run"
+      div_ [class_ "w-36 flex items-center justify-center"] $ span_ [class_ "font-base"] "Schedule"
+    div_ [class_ "w-full flex flex-row p-3"] $ do
+      div_ [class_ "relative flex w-full bg-white py-2 px-3 border-solid border border-gray-200 h-10"] $ do
+        faIcon_ "fa-magnifying-glass" "fa-light fa-magnifying-glass" "h-5 w-5"
+        input_
+          [ type_ "text"
+          , [__| on input show .endpoint_item in #endpoints_container when its textContent.toLowerCase() contains my value.toLowerCase() |]
+          , class_ "dataTable-search w-full h-full p-2 text-gray-500 font-normal focus:outline-none"
+          , placeholder_ "Search endpoints..."
+          ]
+    forM_ colls \c -> do
+      collectionCard pid c
+
 
 
 collectionCard :: Projects.ProjectId -> Testing.CollectionListItem -> Html ()
 collectionCard pid col = do
-  div_ [class_ "rounded-xl border p-4 flex flex-col gap-5 text-gray-700 h-full shadow hover:shadow-lg"] $ do
-    a_ [href_ ("/p/" <> pid.toText <> "/testing/" <> col.id.toText)] $ do
-      div_ [class_ "flex flex-col gap-5"] $ do
-        div_ [class_ "flex items-center justify-between"] $ do
-          div_ [class_ "flex flex-col gap-1"] $ do
-            span_ [class_ "text-sm font-medium"] "Created at"
-            span_ [class_ "text-xs text-gray-500"] $ toHtml $ T.take 19 $ show @Text col.createdAt
-          div_ [class_ "flex flex-col gap-1"] $ do
-            span_ [class_ "text-sm font-medium"] "Last modified"
+  div_ [class_ "flex py-4 gap-8 items-center endpoint_item"] do
+    div_ [class_ "h-4 flex space-x-3 w-8 "] do
+      a_ [class_ " w-2 h-full"] ""
+      input_ [term "aria-label" "Select Issue", class_ "endpoint_anomaly_input", type_ "checkbox"]
+    div_ [class_ "space-y-3 grow"] do
+      div_ [class_ "space-x-3"] do
+        a_ [class_ "inline-block font-bold text-blue-700 space-x-2"] $ do
+          span_ $ toHtml col.title
+        small_ [class_ "inline-block text-gray-800"] $ toHtml $ T.take 19 $ show @Text col.createdAt
+        a_ [href_ ("/p/" <> pid.toText <> "/testing/" <> col.id.toText)] $ do
+          span_ [class_ "text-blue-500"] "View Details"
+        div_ [class_ "flex items-center gap-2 mt-5"] do  
             span_ [class_ "text-xs text-gray-500"] $ toHtml $ T.take 19 $ show @Text col.updatedAt
-        div_ [class_ "flex flex-col w-full gap-2"] $ do
-          h3_ [class_ "font-semibold tracking-tight text-xl"] $ toHtml col.title
-          p_ [class_ "text-sm text-gray-500 break-words max-w-4xl"] $ toHtml col.description
-          div_ [class_ "flex justify-between items-center"] do
-            div_ [class_ "flex gap-2 items-center text-xs rounded py-1"] $ do
-              span_ [class_ "font-bold"] "Last run"
-              span_ [class_ "text-gray-500"] $ toHtml $ maybe "-" (T.take 19 . show @Text) col.lastRun
-            div_ [class_ "flex gap-2 items-center text-xs rounded py-1"] $ do
-              span_ [class_ "font-bold"] "Schedule"
-              span_ [class_ "text-gray-500"] $ toHtml $ maybe "-" (T.take 19 . show @Text) col.lastRun
-    div_ [class_ "text-sm flex items-center justify-between"] $ do
-      div_ [class_ "flex gap-5 items-center"] $ do
-        div_ [class_ "flex gap-2  rounded bg-gray-100 px-2 py-1"] $ do
-          span_ "Steps"
-          span_ [class_ "text-blue-500 font-medium"] $ show col.stepsCount
-        div_ [class_ "flex gap-2 rounded bg-gray-100 px-2 py-1"] $ do
-          span_ "Passed"
-          span_ [class_ "text-green-500 font-medium"] "-"
-        div_ [class_ "flex gap-2 rounded bg-gray-100 px-2 py-1"] $ do
-          span_ "Failed"
-          span_ [class_ "text-red-500 font-medium"] "-"
+            span_ [class_ "text-gray-500"] $ toHtml $ maybe "-" (T.take 19 . show @Text) col.lastRun
+           
+              
+    div_ [class_ "flex items-center justify-center "] do
+      div_ [class_ "grid grid-cols-3 gap-2 text-center text-xs w-96"] do
+        div_ [class_ "p-2 bg-emerald-100 text-emerald-900 border border-emerald-300"] do
+          div_ [class_ "text-base"] $ show col.stepsCount
+          small_ [class_ "block"] "Steps"
+        div_ [class_ " p-2 bg-slate-100 text-slate-900 border border-slate-300"] do
+          div_ [class_ "text-base"] "-"
+          small_ [class_ "block"] "Passed"
+        div_ [class_ "p-2  bg-rose-100 text-rose-900 border border-rose-300"] do
+          div_ [class_ "text-base"] "-"
+          small_ [class_ "block"] "Failed"
+    div_ [class_ "w-36 flex items-center justify-center"] do
       button_
         [ term "data-id" col.id.toText
         , term "data-title" col.title
@@ -231,3 +269,7 @@ modal pid = do
             event.stopPropagation();
         }
       |]
+
+
+
+      
