b261504961993939d4449171f8e169e4f0799e2f
dawkaka <yousiph77@gmail.com>
2024-06-05 18:03:25 +0000

    format

--- src/Models/Tests/Testing.hs
+++ src/Models/Tests/Testing.hs
@@ -1,24 +1,24 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 
-module Models.Tests.Testing
-  ( Collection (..),
-    StepResult (..),
-    StepRequest (..),
-    StepResponse (..),
-    CollectionId (..),
-    CollectionListItem (..),
-    CollectionStepId (..),
-    CollectionStep (..),
-    CollectionStepData (..),
-    CollectionSteps (..),
-    stepDataMethod,
-    updateCollection,
-    addCollection,
-    getCollections,
-    getCollectionById,
-    getCollectionsId,
-    TabStatus (..),
-  )
+module Models.Tests.Testing (
+  Collection (..),
+  StepResult (..),
+  StepRequest (..),
+  StepResponse (..),
+  CollectionId (..),
+  CollectionListItem (..),
+  CollectionStepId (..),
+  CollectionStep (..),
+  CollectionStepData (..),
+  CollectionSteps (..),
+  stepDataMethod,
+  updateCollection,
+  addCollection,
+  getCollections,
+  getCollectionById,
+  getCollectionsId,
+  TabStatus (..),
+)
 where
 
 import Data.Aeson (KeyValue ((.=)), (.:?))
@@ -43,6 +43,7 @@ import Models.Projects.Projects qualified as Projects
 import Relude hiding (get, put)
 import Web.HttpApiData (FromHttpApiData)
 
+
 newtype CollectionId = CollectionId {collectionId :: UUID.UUID}
   deriving stock (Generic, Show)
   deriving
@@ -50,8 +51,10 @@ newtype CollectionId = CollectionId {collectionId :: UUID.UUID}
     via UUID.UUID
   deriving anyclass (FromRow, ToRow)
 
+
 instance HasField "toText" CollectionId Text where
   getField colid = UUID.toText colid.collectionId
+
 
 newtype CollectionStepId = CollectionStepId {collectionStepId :: UUID.UUID}
   deriving stock (Generic, Show)
@@ -59,59 +62,64 @@ newtype CollectionStepId = CollectionStepId {collectionStepId :: UUID.UUID}
     (Eq, Ord, AE.ToJSON, AE.FromJSON, FromField, ToField, FromHttpApiData, Default, NFData)
     via UUID.UUID
 
+
 instance HasField "toText" CollectionStepId Text where
   getField = UUID.toText . collectionStepId
 
+
 data CollectionStepData = CollectionStepData
-  { title :: Maybe Text,
-    post :: Maybe Text,
-    get :: Maybe Text,
-    update :: Maybe Text,
-    delete :: Maybe Text,
-    patch :: Maybe Text,
-    put :: Maybe Text,
-    params :: Maybe (Map Text Text),
-    headers :: Maybe (Map Text Text),
-    exports :: Maybe (Map Text Text),
-    json :: Maybe (AE.Value),
-    raw :: Maybe (Text),
-    asserts :: Maybe (V.Vector (Map Text AE.Value))
+  { title :: Maybe Text
+  , post :: Maybe Text
+  , get :: Maybe Text
+  , update :: Maybe Text
+  , delete :: Maybe Text
+  , patch :: Maybe Text
+  , put :: Maybe Text
+  , params :: Maybe (Map Text Text)
+  , headers :: Maybe (Map Text Text)
+  , exports :: Maybe (Map Text Text)
+  , json :: Maybe (AE.Value)
+  , raw :: Maybe (Text)
+  , asserts :: Maybe (V.Vector (Map Text AE.Value))
   }
   deriving stock (Show, Generic)
   deriving anyclass (NFData, Default)
   deriving (FromField) via Aeson CollectionStepData
   deriving (ToField) via Aeson CollectionStepData
 
+
 stepDataMethod :: CollectionStepData -> Maybe (Text, Text)
 stepDataMethod stepData =
   asum
     . map (\(method, url) -> fmap (\u -> (method, u)) url)
-    $ [ ("GET", stepData.get),
-        ("POST", stepData.post),
-        ("UPDATE", stepData.update),
-        ("DELETE", stepData.delete),
-        ("PATCH", stepData.patch),
-        ("PUT", stepData.put)
+    $ [ ("GET", stepData.get)
+      , ("POST", stepData.post)
+      , ("UPDATE", stepData.update)
+      , ("DELETE", stepData.delete)
+      , ("PATCH", stepData.patch)
+      , ("PUT", stepData.put)
       ]
+
 
 instance AE.ToJSON CollectionStepData where
   toJSON csd =
     AE.object
       $ catMaybes
-        [ Just $ "title" .= csd.title,
-          fmap ("POST" .=) csd.post, -- Change the key to "POST" here for the output JSON
-          fmap ("GET" .=) csd.get,
-          fmap ("UPDATE" .=) csd.update,
-          fmap ("DELETE" .=) csd.delete,
-          fmap ("PATCH" .=) csd.patch,
-          fmap ("PUT" .=) csd.put,
-          fmap ("params" .=) csd.params,
-          fmap ("headers" .=) csd.headers,
-          fmap ("exports" .=) csd.exports,
-          fmap ("json" .=) csd.json,
-          fmap ("raw" .=) csd.raw,
-          fmap ("asserts" .=) csd.asserts
+        [ Just $ "title" .= csd.title
+        , fmap ("POST" .=) csd.post -- Change the key to "POST" here for the output JSON
+        , fmap ("GET" .=) csd.get
+        , fmap ("UPDATE" .=) csd.update
+        , fmap ("DELETE" .=) csd.delete
+        , fmap ("PATCH" .=) csd.patch
+        , fmap ("PUT" .=) csd.put
+        , fmap ("params" .=) csd.params
+        , fmap ("headers" .=) csd.headers
+        , fmap ("exports" .=) csd.exports
+        , fmap ("json" .=) csd.json
+        , fmap ("raw" .=) csd.raw
+        , fmap ("asserts" .=) csd.asserts
         ]
+
 
 instance AE.FromJSON CollectionStepData where
   parseJSON = AE.withObject "CollectionStepData" $ \v -> do
@@ -128,17 +136,18 @@ instance AE.FromJSON CollectionStepData where
     json <- v .:? "json"
     raw <- v .:? "raw"
     asserts <- v .:? "asserts"
-    return CollectionStepData {..}
+    return CollectionStepData{..}
+
 
 -- TODO: delete table
 data CollectionStep = CollectionStep
-  { id :: CollectionStepId,
-    createdAt :: ZonedTime,
-    updatedAt :: ZonedTime,
-    lastRun :: Maybe ZonedTime,
-    projectId :: Projects.ProjectId,
-    collectionId :: CollectionId,
-    stepData :: CollectionStepData
+  { id :: CollectionStepId
+  , createdAt :: ZonedTime
+  , updatedAt :: ZonedTime
+  , lastRun :: Maybe ZonedTime
+  , projectId :: Projects.ProjectId
+  , collectionId :: CollectionId
+  , stepData :: CollectionStepData
   }
   deriving stock (Show, Generic)
   deriving anyclass (FromRow, ToRow, AE.ToJSON, AE.FromJSON, NFData, Default)
@@ -146,23 +155,25 @@ data CollectionStep = CollectionStep
     (Entity)
     via (GenericEntity '[Schema "tests", TableName "collection_steps", PrimaryKey "id", FieldModifiers '[CamelToSnake]] CollectionStep)
 
+
 newtype CollectionSteps = CollectionSteps (V.Vector CollectionStepData)
   deriving stock (Show, Generic)
   deriving anyclass (AE.ToJSON, AE.FromJSON, NFData, Default)
   deriving (FromField, ToField) via Aeson (CollectionSteps)
 
+
 data Collection = Collection
-  { id :: CollectionId,
-    createdAt :: ZonedTime,
-    updatedAt :: ZonedTime,
-    lastRun :: Maybe ZonedTime,
-    projectId :: Projects.ProjectId,
-    title :: Text,
-    description :: Text,
-    config :: AE.Value,
-    schedule :: Text,
-    isScheduled :: Bool,
-    collectionSteps :: CollectionSteps
+  { id :: CollectionId
+  , createdAt :: ZonedTime
+  , updatedAt :: ZonedTime
+  , lastRun :: Maybe ZonedTime
+  , projectId :: Projects.ProjectId
+  , title :: Text
+  , description :: Text
+  , config :: AE.Value
+  , schedule :: Text
+  , isScheduled :: Bool
+  , collectionSteps :: CollectionSteps
   }
   deriving stock (Show, Generic)
   deriving anyclass (FromRow, ToRow, AE.ToJSON, AE.FromJSON, NFData, Default)
@@ -170,17 +181,18 @@ data Collection = Collection
     (Entity)
     via (GenericEntity '[Schema "tests", TableName "collections", PrimaryKey "id", FieldModifiers '[CamelToSnake]] Collection)
 
+
 data CollectionListItem = ReportListItem
-  { id :: CollectionId,
-    createdAt :: ZonedTime,
-    updatedAt :: ZonedTime,
-    projectId :: Projects.ProjectId,
-    lastRun :: Maybe ZonedTime,
-    title :: Text,
-    description :: Text,
-    stepsCount :: Int,
-    schedule :: Text,
-    isScheduled :: Bool
+  { id :: CollectionId
+  , createdAt :: ZonedTime
+  , updatedAt :: ZonedTime
+  , projectId :: Projects.ProjectId
+  , lastRun :: Maybe ZonedTime
+  , title :: Text
+  , description :: Text
+  , stepsCount :: Int
+  , schedule :: Text
+  , isScheduled :: Bool
   }
   deriving stock (Show, Generic)
   deriving anyclass (FromRow, ToRow, NFData)
@@ -188,47 +200,55 @@ data CollectionListItem = ReportListItem
     (Entity)
     via (GenericEntity '[Schema "tests", TableName "collections", PrimaryKey "id", FieldModifiers '[CamelToSnake]] CollectionListItem)
 
+
 data StepResponse = StepResponse
-  { status :: Int,
-    headers :: Map Text [Text],
-    raw :: Text,
-    json :: AE.Value
+  { status :: Int
+  , headers :: Map Text [Text]
+  , raw :: Text
+  , json :: AE.Value
   }
   deriving stock (Show, Generic)
   deriving (AE.FromJSON, AE.ToJSON) via DAE.CustomJSON '[DAE.FieldLabelModifier '[DAE.CamelToSnake]] StepResponse
 
+
 data StepRequest = StepRequest
-  { req :: CollectionStepData,
-    resp :: StepResponse
+  { req :: CollectionStepData
+  , resp :: StepResponse
   }
   deriving stock (Show, Generic)
   deriving (AE.FromJSON, AE.ToJSON) via DAE.CustomJSON '[DAE.FieldLabelModifier '[DAE.CamelToSnake]] StepRequest
+
 
 data AssertResult = AssertResult
   {}
   deriving stock (Show, Generic)
   deriving (AE.FromJSON, AE.ToJSON) via DAE.CustomJSON '[DAE.FieldLabelModifier '[DAE.CamelToSnake]] AssertResult
 
+
 data StepResult = StepResult
-  { stepName :: Maybe Text,
-    stepIndex :: Int,
-    assertResults :: [AssertResult],
-    request :: StepRequest,
-    stepLog :: Text
+  { stepName :: Maybe Text
+  , stepIndex :: Int
+  , assertResults :: [AssertResult]
+  , request :: StepRequest
+  , stepLog :: Text
   }
   deriving stock (Show, Generic)
   deriving (AE.FromJSON, AE.ToJSON) via DAE.CustomJSON '[DAE.FieldLabelModifier '[DAE.CamelToSnake]] StepResult
 
+
 data TabStatus = Active | Inactive
+
 
 addCollection :: Collection -> DBT IO ()
 addCollection = insert @Collection
+
 
 updateCollection :: Projects.ProjectId -> CollectionId -> Text -> Text -> Bool -> Text -> V.Vector CollectionStepData -> DBT IO Int64
 updateCollection pid cid title description scheduled scheduleInterval collectionSteps = execute Update q params
   where
     params = (title, description, scheduleInterval, scheduled, CollectionSteps collectionSteps, pid, cid)
     q = [sql| UPDATE tests.collections SET title=?, description=?, schedule=?, is_scheduled=?,  collection_steps=? WHERE project_id=? AND id=? |]
+
 
 getCollectionById :: CollectionId -> DBT IO (Maybe Collection)
 getCollectionById id' = queryOne Select q (Only id')
@@ -241,6 +261,7 @@ getCollectionById id' = queryOne Select q (Only id')
                       ELSE CONCAT(EXTRACT(MINUTE FROM schedule)::TEXT, ' minutes')
                   END as schedule, is_scheduled, collection_steps 
                   FROM tests.collections t WHERE id=?|]
+
 
 -- TODO: delete or remove the collect_steps join
 getCollections :: Projects.ProjectId -> TabStatus -> DBT IO (V.Vector CollectionListItem)
@@ -265,6 +286,7 @@ getCollections pid tabStatus = query Select q (pid, statusValue)
       GROUP BY t.id
       ORDER BY t.updated_at DESC;
     |]
+
 
 getCollectionsId :: DBT IO (V.Vector CollectionId)
 getCollectionsId = query Select q ()
