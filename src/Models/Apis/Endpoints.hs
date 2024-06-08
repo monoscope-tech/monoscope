3c6a3bcc478e5ddbb0dff8a5c058f268015971fc
dawkaka <yousiph77@gmail.com>
2024-06-08 13:38:36 +0000

    fix outbound integrations page

--- src/Models/Apis/Endpoints.hs
+++ src/Models/Apis/Endpoints.hs
@@ -3,28 +3,28 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RankNTypes #-}
 
-module Models.Apis.Endpoints (
-  Endpoint (..),
-  SwEndpoint (..),
-  EndpointId (..),
-  EndpointRequestStats (..),
-  Host (..),
-  HostEvents (..),
-  dependenciesAndEventsCount,
-  endpointsByProjectId,
-  endpointUrlPath,
-  endpointRequestStatsByProject,
-  dependencyEndpointsRequestStatsByProject,
-  endpointRequestStatsByEndpoint,
-  endpointById,
-  endpointIdText,
-  endpointToUrlPath,
-  upsertEndpointQueryAndParam,
-  endpointByHash,
-  getProjectHosts,
-  insertEndpoints,
-  countEndpointInbox,
-)
+module Models.Apis.Endpoints
+  ( Endpoint (..),
+    SwEndpoint (..),
+    EndpointId (..),
+    EndpointRequestStats (..),
+    Host (..),
+    HostEvents (..),
+    dependenciesAndEventsCount,
+    endpointsByProjectId,
+    endpointUrlPath,
+    endpointRequestStatsByProject,
+    dependencyEndpointsRequestStatsByProject,
+    endpointRequestStatsByEndpoint,
+    endpointById,
+    endpointIdText,
+    endpointToUrlPath,
+    upsertEndpointQueryAndParam,
+    endpointByHash,
+    getProjectHosts,
+    insertEndpoints,
+    countEndpointInbox,
+  )
 where
 
 import Data.Aeson (FromJSON, ToJSON)
@@ -52,54 +52,46 @@ import Relude
 import Utils (DBField (MkDBField))
 import Web.HttpApiData (FromHttpApiData)
 
-
 newtype EndpointId = EndpointId {unEndpointId :: UUID.UUID}
   deriving stock (Generic, Show)
   deriving newtype (ToJSON, FromJSON, Eq, Ord, FromField, ToField, FromHttpApiData, Default, NFData)
   deriving anyclass (FromRow, ToRow)
 
-
 newtype Host = Host {host :: Text}
   deriving stock (Show, Generic, Eq)
   deriving anyclass (FromRow, ToRow, Default, NFData)
 
-
 instance HasField "toText" EndpointId Text where
   getField = UUID.toText . unEndpointId
-
 
 endpointIdText :: EndpointId -> Text
 endpointIdText = UUID.toText . unEndpointId
 
-
 -- TODO: Introduce request header hashes and response header hashes
 data Endpoint = Endpoint
-  { id :: EndpointId
-  , createdAt :: UTCTime
-  , updatedAt :: UTCTime
-  , projectId :: Projects.ProjectId
-  , urlPath :: Text
-  , urlParams :: AE.Value -- Key value map of key to the type. Needs a bit more figuring out.
-  , method :: Text
-  , host :: Text
-  , hash :: Text
-  , outgoing :: Bool
-  , description :: Text
+  { id :: EndpointId,
+    createdAt :: UTCTime,
+    updatedAt :: UTCTime,
+    projectId :: Projects.ProjectId,
+    urlPath :: Text,
+    urlParams :: AE.Value, -- Key value map of key to the type. Needs a bit more figuring out.
+    method :: Text,
+    host :: Text,
+    hash :: Text,
+    outgoing :: Bool,
+    description :: Text
   }
   deriving stock (Show, Generic, Eq)
   deriving anyclass (FromRow, ToRow, Default, NFData)
   deriving (AE.FromJSON) via DAE.CustomJSON '[DAE.OmitNothingFields, DAE.FieldLabelModifier '[DAE.CamelToSnake]] Endpoint
   deriving (FromField) via Aeson Endpoint
 
-
 -- | endpointToUrlPath builds an apitoolkit path link to the endpoint details page of that endpoint.
 endpointToUrlPath :: Endpoint -> Text
 endpointToUrlPath enp = endpointUrlPath enp.projectId enp.id
 
-
 endpointUrlPath :: Projects.ProjectId -> EndpointId -> Text
 endpointUrlPath pid eid = "/p/" <> pid.toText <> "/endpoints/" <> endpointIdText eid
-
 
 upsertEndpointQueryAndParam :: Endpoint -> (Query, [DBField])
 upsertEndpointQueryAndParam endpoint = (q, params)
@@ -113,44 +105,42 @@ upsertEndpointQueryAndParam endpoint = (q, params)
           DO  NOTHING;
       |]
     params =
-      [ MkDBField endpoint.projectId
-      , MkDBField endpoint.urlPath
-      , MkDBField endpoint.urlParams
-      , MkDBField endpoint.method
-      , MkDBField host
-      , MkDBField endpoint.hash
-      , MkDBField endpoint.outgoing
+      [ MkDBField endpoint.projectId,
+        MkDBField endpoint.urlPath,
+        MkDBField endpoint.urlParams,
+        MkDBField endpoint.method,
+        MkDBField host,
+        MkDBField endpoint.hash,
+        MkDBField endpoint.outgoing
       ]
-
 
 -- Based of a view which is generated every 5minutes.
 data EndpointRequestStats = EndpointRequestStats
-  { endpointId :: EndpointId
-  , endpointHash :: Text
-  , projectId :: Projects.ProjectId
-  , urlPath :: Text
-  , method :: Text
-  , min :: Double
-  , p50 :: Double
-  , p75 :: Double
-  , p90 :: Double
-  , p95 :: Double
-  , p99 :: Double
-  , max :: Double
-  , totalTime :: Double
-  , totalTimeProj :: Double
-  , totalRequests :: Int
-  , totalRequestsProj :: Int
-  , ongoingAnomalies :: Int
-  , ongoingAnomaliesProj :: Int
-  , acknowlegedAt :: Maybe UTCTime
-  , archivedAt :: Maybe UTCTime
-  , anomalyId :: UUID.UUID
+  { endpointId :: EndpointId,
+    endpointHash :: Text,
+    projectId :: Projects.ProjectId,
+    urlPath :: Text,
+    method :: Text,
+    min :: Double,
+    p50 :: Double,
+    p75 :: Double,
+    p90 :: Double,
+    p95 :: Double,
+    p99 :: Double,
+    max :: Double,
+    totalTime :: Double,
+    totalTimeProj :: Double,
+    totalRequests :: Int,
+    totalRequestsProj :: Int,
+    ongoingAnomalies :: Int,
+    ongoingAnomaliesProj :: Int,
+    acknowlegedAt :: Maybe UTCTime,
+    archivedAt :: Maybe UTCTime,
+    anomalyId :: UUID.UUID
   }
   deriving stock (Show, Generic, Eq)
   deriving anyclass (FromRow, ToRow, Default, NFData)
   deriving (Entity) via (GenericEntity '[Schema "apis", TableName "endpoint_request_stats", PrimaryKey "endpoint_id", FieldModifiers '[CamelToSnake]] EndpointRequestStats)
-
 
 -- FIXME: Include and return a boolean flag to show if fields that have annomalies.
 -- FIXME: return endpoint_hash as well.
@@ -184,7 +174,6 @@ endpointRequestStatsByProject pid ackd archived pHostM sortM = case pHostM of Ju
      order by $orderBy , url_path ASC;
   |]
 
-
 dependencyEndpointsRequestStatsByProject :: Projects.ProjectId -> Text -> PgT.DBT IO (Vector EndpointRequestStats)
 dependencyEndpointsRequestStatsByProject pid host = query Select (Query $ encodeUtf8 q) (pid, host)
   where
@@ -208,7 +197,6 @@ dependencyEndpointsRequestStatsByProject pid host = query Select (Query $ encode
      order by total_requests DESC, enp.url_path ASC;
     |]
 
-
 -- FIXME: return endpoint_hash as well.
 -- This would require tampering with the view.
 endpointRequestStatsByEndpoint :: EndpointId -> PgT.DBT IO (Maybe EndpointRequestStats)
@@ -226,42 +214,37 @@ endpointRequestStatsByEndpoint eid = queryOne Select q (eid, eid)
                   null, null, '00000000-0000-0000-0000-000000000000'::uuid
               FROM apis.endpoint_request_stats WHERE endpoint_id=?|]
 
-
 endpointById :: EndpointId -> PgT.DBT IO (Maybe Endpoint)
 endpointById eid = queryOne Select q (Only eid)
   where
     q = [sql| SELECT id, created_at, updated_at, project_id, url_path, url_params, method, host, hash, outgoing, description from apis.endpoints where id=? |]
-
 
 endpointByHash :: Projects.ProjectId -> Text -> PgT.DBT IO (Maybe Endpoint)
 endpointByHash pid hash = queryOne Select q (pid, hash)
   where
     q = [sql| SELECT id, created_at, updated_at, project_id, url_path, url_params, method, host, hash, outgoing, description from apis.endpoints where project_id=? AND hash=? |]
 
-
 data SwEndpoint = SwEndpoint
-  { urlPath :: Text
-  , urlParams :: AE.Value -- Key value map of key to the type. Needs a bit more figuring out.
-  , method :: Text
-  , host :: Text
-  , hash :: Text
-  , description :: Text
+  { urlPath :: Text,
+    urlParams :: AE.Value, -- Key value map of key to the type. Needs a bit more figuring out.
+    method :: Text,
+    host :: Text,
+    hash :: Text,
+    description :: Text
   }
   deriving stock (Show, Generic, Eq)
   deriving anyclass (FromRow, ToRow, Default, NFData)
   deriving (AE.FromJSON) via DAE.CustomJSON '[DAE.OmitNothingFields, DAE.FieldLabelModifier '[DAE.CamelToSnake]] SwEndpoint
   deriving (FromField) via Aeson SwEndpoint
 
-
 data HostEvents = HostEvents
-  { host :: Text
-  , eventCount :: Integer
-  , first_seen :: Maybe ZonedTime
-  , last_seen :: Maybe ZonedTime
+  { host :: Text,
+    eventCount :: Integer,
+    first_seen :: Maybe ZonedTime,
+    last_seen :: Maybe ZonedTime
   }
   deriving stock (Show, Generic, Eq)
   deriving anyclass (ToRow, FromRow, NFData)
-
 
 endpointsByProjectId :: Projects.ProjectId -> PgT.DBT IO (Vector SwEndpoint)
 endpointsByProjectId pid = query Select q (Only pid)
@@ -274,7 +257,6 @@ endpointsByProjectId pid = query Select q (Only pid)
          apis.anomalies ann ON (ann.anomaly_type = 'endpoint' AND ann.target_hash = enp.hash)
          WHERE enp.project_id = ? AND ann.acknowleged_at IS NOT NULL
        |]
-
 
 insertEndpoints :: [Endpoint] -> DBT IO Int64
 insertEndpoints endpoints = do
@@ -290,24 +272,22 @@ insertEndpoints endpoints = do
   where
     getEndpointParams :: Endpoint -> (Projects.ProjectId, Text, AE.Value, Text, Text, Text, Text)
     getEndpointParams endpoint =
-      ( endpoint.projectId
-      , endpoint.urlPath
-      , endpoint.urlParams
-      , endpoint.method
-      , endpoint.host
-      , endpoint.hash
-      , endpoint.description
+      ( endpoint.projectId,
+        endpoint.urlPath,
+        endpoint.urlParams,
+        endpoint.method,
+        endpoint.host,
+        endpoint.hash,
+        endpoint.description
       )
-
 
 getProjectHosts :: Projects.ProjectId -> PgT.DBT IO (Vector Host)
 getProjectHosts pid = query Select q (Only pid)
   where
     q = [sql| SELECT DISTINCT host FROM apis.endpoints where  project_id = ? AND outgoing=false AND host!= '' |]
 
-
 dependenciesAndEventsCount :: Projects.ProjectId -> Text -> DBT IO (Vector HostEvents)
-dependenciesAndEventsCount pid sortT = query Select (Query $ encodeUtf8 q) (pid, pid, pid, pid)
+dependenciesAndEventsCount pid sortT = query Select (Query $ encodeUtf8 q) (pid, pid)
   where
     orderBy = case sortT of
       "first_seen" -> "first_seen ASC;"
@@ -316,35 +296,28 @@ dependenciesAndEventsCount pid sortT = query Select (Query $ encodeUtf8 q) (pid,
 
     q =
       [text|
+WITH filtered_requests AS (
+    SELECT rd.host,
+           COUNT(*) AS eventsCount,
+           MAX(rd.created_at) AS last_seen,
+           MIN(rd.created_at) AS first_seen
+    FROM apis.request_dumps rd
+    WHERE rd.project_id = ?
+      AND rd.created_at > NOW() - interval '14' day
+      AND rd.request_type = 'Outgoing'
+    GROUP BY rd.host
+)
 SELECT DISTINCT ep.host,
-       (SELECT COUNT(*)
-        FROM apis.request_dumps rd
-        WHERE rd.project_id = ?
-          AND rd.created_at > NOW() - interval '14' day
-          AND rd.host = ep.host
-          AND rd.request_type = 'Outgoing'
-       ) AS eventsCount,
-       (SELECT MAX(rd.created_at)
-        FROM apis.request_dumps rd
-        WHERE rd.project_id = ?
-          AND rd.created_at > NOW() - interval '14' day
-          AND rd.host = ep.host
-          AND rd.request_type = 'Outgoing'
-       ) AS last_seen,
-       (SELECT MIN(rd.created_at)
-        FROM apis.request_dumps rd
-        WHERE rd.project_id = ?
-          AND rd.created_at > NOW() - interval '14' day
-          AND rd.host = ep.host
-          AND rd.request_type = 'Outgoing'
-       ) AS first_seen
+       fr.eventsCount,
+       fr.last_seen last_seen,
+       fr.first_seen first_seen
 FROM apis.endpoints ep
+LEFT JOIN filtered_requests fr ON ep.host = fr.host
 WHERE ep.project_id = ?
   AND ep.host != ''
   AND ep.outgoing = true
-ORDER BY $orderBy
+  ORDER BY $orderBy
       |]
-
 
 countEndpointInbox :: Projects.ProjectId -> DBT IO Int
 countEndpointInbox pid = do
--- src/Pages/Endpoints/Outgoing.hs
+++ src/Pages/Endpoints/Outgoing.hs
@@ -18,42 +18,36 @@ import Relude hiding (ask, asks)
 import System.Types
 import Utils (faSprite_, mIcon_)
 
-
 outgoingGetH :: Projects.ProjectId -> Maybe Text -> ATAuthCtx (RespHeaders (Html ()))
 outgoingGetH pid sortM = do
   (sess, project) <- Sessions.sessionAndProject pid
   hostsAndEvents <- dbtToEff $ Endpoints.dependenciesAndEventsCount pid (fromMaybe "events" sortM)
   let bwconf =
         def
-          { sessM = Just sess.persistentSession
-          , currProject = Just project
-          , pageTitle = "Outgoing Integrations"
+          { sessM = Just sess.persistentSession,
+            currProject = Just project,
+            pageTitle = "Outgoing Integrations"
           }
   addRespHeaders $ bodyWrapper bwconf $ outgoingPage pid (fromMaybe "events" sortM) hostsAndEvents
 
-
 sortOptions :: [(Text, Text, Text)]
 sortOptions =
-  [ ("First Seen", "First time the issue occured", "first_seen")
-  , ("Last Seen", "Last time the issue occured", "last_seen")
-  , ("Events", "Number of events", "events")
+  [ ("First Seen", "First time the issue occured", "first_seen"),
+    ("Last Seen", "Last time the issue occured", "last_seen"),
+    ("Events", "Number of events", "events")
   ]
-
 
 outgoingPage :: Projects.ProjectId -> Text -> V.Vector Endpoints.HostEvents -> Html ()
 outgoingPage pid sortV hostsEvents = do
   div_ [class_ "w-full mx-auto px-16 pt-10 pb-24 overflow-y-scroll h-full"] $ do
     h3_ [class_ "text-xl text-slate-700 flex gap-1 place-items-center mb-10"] "Outbound Integrations"
-    div_ [class_ "grid grid-cols-5 card-round", id_ "anomalyListBelowTab", hxTrigger_ "refreshMain"] $ outgoingList' pid sortV hostsEvents
-
+    div_ [class_ "grid grid-cols-5 card-round overflow-hidden", id_ "anomalyListBelowTab", hxTrigger_ "refreshMain"] $ outgoingList' pid sortV hostsEvents
 
 outgoingList' :: Projects.ProjectId -> Text -> V.Vector Endpoints.HostEvents -> Html ()
-outgoingList' pid sortV hostsEvents = form_ [class_ "col-span-5 bg-white divide-y ", id_ "anomalyListForm"] $ do
-  div_ [class_ "flex py-3 gap-8 items-center  bg-gray-50"] do
-    div_ [class_ "h-4 flex space-x-3 w-8"] do
-      a_ [class_ " w-2 h-full"] "" >> input_ [term "aria-label" "Select Issue", type_ "checkbox"]
-    div_ [class_ " grow flex flex-row gap-2"] do
-      button_ [class_ "btn btn-sm btn-outline border-black hover:shadow-2xl", hxSwap_ "none"] "HOST"
+outgoingList' pid sortV hostsEvents = div_ [class_ "col-span-5 bg-white divide-y ", id_ "anomalyListForm"] $ do
+  div_ [class_ "flex py-3 px-3 gap-8 items-center  bg-gray-50 overflow-hidden"] do
+    div_ [class_ " grow flex flex-row gap-2 font-bold"] do
+      "HOST"
     div_ [class_ "relative inline-block"] do
       let currentSortTitle = maybe "Events" fst3 $ find (\(_, _, identifier) -> identifier == sortV) sortOptions
       a_ [class_ "btn-sm bg-transparent border-black hover:shadow-2xl space-x-2 cursor-pointer", [__|on click toggle .hidden on #sortMenuDiv |]]
@@ -63,9 +57,9 @@ outgoingList' pid sortV hostsEvents = form_ [class_ "col-span-5 bg-white divide-
         sortOptions & mapM_ \(title, desc, identifier) -> do
           let isActive = sortV == identifier
           a_
-            [ class_ $ "block flex flex-row px-3 py-2 hover:bg-blue-50 rounded-md cursor-pointer " <> (if isActive then " text-blue-800 " else "")
-            , href_ $ "/p/" <> pid.toText <> "/outgoing?sort=" <> identifier
-            , hxIndicator_ "#sortLoading"
+            [ class_ $ "block flex flex-row px-3 py-2 hover:bg-blue-50 rounded-md cursor-pointer " <> (if isActive then " text-blue-800 " else ""),
+              href_ $ "/p/" <> pid.toText <> "/outgoing?sort=" <> identifier,
+              hxIndicator_ "#sortLoading"
             ]
             do
               div_ [class_ "flex flex-col items-center justify-center px-3"]
@@ -86,10 +80,10 @@ outgoingList' pid sortV hostsEvents = form_ [class_ "col-span-5 bg-white divide-
     div_ [class_ "relative flex w-full bg-white py-2 px-3 border-solid border border-gray-200 h-10"] $ do
       faSprite_ "magnifying-glass" "regular" "h-5 w-5"
       input_
-        [ type_ "text"
-        , [__| on input show .endpoint_item in #endpoints_container when its textContent.toLowerCase() contains my value.toLowerCase() |]
-        , class_ "dataTable-search w-full h-full p-2 text-gray-500 font-normal focus:outline-none"
-        , placeholder_ "Search endpoints..."
+        [ type_ "text",
+          [__|on input show .host in #hosts_container when its textContent.toLowerCase() contains my value.toLowerCase() |],
+          class_ "dataTable-search w-full h-full p-2 text-gray-500 font-normal focus:outline-none",
+          placeholder_ "Search outbound integrations..."
         ]
   when (null hostsEvents) $ section_ [class_ "mx-auto w-max p-5 sm:py-10 sm:px-16 items-center flex my-10 gap-16"] do
     div_ [] do
@@ -100,15 +94,11 @@ outgoingList' pid sortV hostsEvents = form_ [class_ "col-span-5 bg-white divide-
       a_ [href_ $ "/p/" <> pid.toText <> "/integration_guides#outgoing-request-monitoring", class_ "w-max btn btn-indigo -ml-1 text-md"] "See monitoring guide"
   renderOutgoing pid hostsEvents
 
-
 renderOutgoing :: Projects.ProjectId -> V.Vector Endpoints.HostEvents -> Html ()
 renderOutgoing pid hostsEvents = do
-  div_ [class_ "flex flex-col"] do
+  div_ [class_ "flex flex-col", id_ "hosts_container"] do
     forM_ hostsEvents $ \host -> do
-      div_ [class_ "flex py-4 gap-8 items-center endpoint_item "] do
-        div_ [class_ "h-4 flex space-x-3 w-8 "] do
-          a_ [class_ " w-2 h-full"] ""
-          input_ [term "aria-label" "Select Issue", class_ "endpoint_anomaly_input", type_ "checkbox"]
+      div_ [class_ "flex py-4 gap-8 items-center hover:bg-gray-100 host"] do
         div_ [class_ "space-y-3 grow"] do
           div_ [class_ "space-x-3"] do
             a_ [class_ "inline-block font-bold space-x-2"] $ do
@@ -116,10 +106,10 @@ renderOutgoing pid hostsEvents = do
               a_ [href_ $ "/p/" <> pid.toText <> "/log_explorer?query=host%3D%3D" <> "\"" <> host.host <> "\"", class_ "text-blue-500 hover:text-slate-600"] $ "View logs"
         div_ [class_ "flex items-center justify-center "]
           $ div_
-            [ class_ "w-56 h-12 px-3"
-            , hxGet_ $ "/charts_html?pid=" <> pid.toText <> "&since=14D&query_raw=" <> AnomalyList.escapedQueryPartial [PyF.fmt|host=="{host.host}" | timechart [1d]|]
-            , hxTrigger_ "intersect once"
-            , hxSwap_ "innerHTML"
+            [ class_ "w-56 h-12 px-3",
+              hxGet_ $ "/charts_html?pid=" <> pid.toText <> "&since=14D&query_raw=" <> AnomalyList.escapedQueryPartial [PyF.fmt|host=="{host.host}" | timechart [1d]|],
+              hxTrigger_ "intersect once",
+              hxSwap_ "innerHTML"
             ]
             ""
         div_ [class_ "w-36 flex items-center justify-center"] $ span_ [class_ "tabular-nums text-xl", term "data-tippy-content" "Events for this Anomaly in the last 14days"] $ toHtml (show host.eventCount)
