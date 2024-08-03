6a97b22d0ef9489648f7d47f9ffb14f7277487fc
dawkaka <yousiph77@gmail.com>
2024-08-03 14:22:51 +0000

    remove discord notif

--- src/ProcessMessage.hs
+++ src/ProcessMessage.hs
@@ -197,17 +197,6 @@ processRequestMessage recMsg = do
   -- we set the value in the db into the cache and return that.
   -- This should help with our performance, since this project Cache is the only information we need in order to process
   -- an apitoolkit requestmessage payload. So we're able to process payloads without hitting the database except for the actual db inserts.
-  let _ =
-        if recMsg.urlPath == Just "/api/intercom/webhook" && recMsg.statusCode /= 500
-          then do
-            let webhookUrl = "https://discord.com/api/webhooks/1230980245423788045/JQOJ7w3gmEduaOvPTnxEz4L8teDpX5PJoFkyQmqZHR8HtRqAkWIjv2Xk1aKadTyXuFy_"
-            let message = object ["content" .= recMsg]
-            let opts = defaults & header "Content-Type" .~ ["application/json"]
-            response <- postWith opts webhookUrl message
-            pass
-          else do
-            pass
-
   projectCacheVal <- liftIO $ Cache.fetchWithCache appCtx.projectCache pid \pid' -> do
     mpjCache <- withPool appCtx.jobsPool $ Projects.projectCacheById pid'
     pure $ fromMaybe projectCacheDefault mpjCache
