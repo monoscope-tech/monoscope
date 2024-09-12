6145658e0909022176a605e63059c26223e83bc3
dawkaka <yousiph77@gmail.com>
2024-09-12 09:54:07 +0000

    log explorer charts

--- src/Pages/Log.hs
+++ src/Pages/Log.hs
@@ -263,22 +263,43 @@ apiLogsPage page = do
       |]
     logQueryBox_ page.pid page.currentRange
 
-    div_ [class_ "card-round w-full grow divide-y flex flex-col text-sm h-full overflow-hidden group/result"] do
+    div_ [class_ "w-full grow divide-y flex flex-col text-sm h-full overflow-hidden group/result"] do
       div_ [class_ "flex-1 "] do
         div_ [class_ "pl-3 py-1 flex flex-row justify-end"] do
           label_ [class_ "flex items-center cursor-pointer space-x-2 p-1"] do
             input_ [type_ "checkbox", class_ "toggle toggle-sm toggle-chart", checked_]
             small_ "toggle chart"
-        div_
-          [ id_ "reqsChartsECP"
-          , class_ "px-5 hidden group-has-[.toggle-chart:checked]/result:block"
-          , style_ "height:150px"
-          , hxGet_ $ "/charts_html?id=reqsChartsEC&show_legend=true&pid=" <> page.pid.toText
-          , hxTrigger_ "intersect,  htmx:beforeRequest from:#log_explorer_form"
-          , hxVals_ "js:{query_raw:window.getQueryFromEditor(), since: getTimeRange().since, from: getTimeRange().from, to:getTimeRange().to, cols:params().cols, layout:'all', source: params().source}"
-          , hxSwap_ "innerHTML"
-          ]
-          ""
+        div_ [class_ "grid grid-cols-3 gap-0"] do
+          div_
+            [ id_ "reqsChartsECP"
+            , class_ "px-5 hidden group-has-[.toggle-chart:checked]/result:block"
+            , style_ "height:150px"
+            , hxGet_ $ "/charts_html?id=reqsChartsEC&show_legend=true&pid=" <> page.pid.toText
+            , hxTrigger_ "intersect,  htmx:beforeRequest from:#log_explorer_form"
+            , hxVals_ "js:{query_raw:window.getQueryFromEditor(), since: getTimeRange().since, from: getTimeRange().from, to:getTimeRange().to, cols:params().cols, layout:'all', source: params().source}"
+            , hxSwap_ "innerHTML"
+            ]
+            ""
+          div_
+            [ id_ "reqsChartsErr"
+            , class_ "px-5 hidden group-has-[.toggle-chart:checked]/result:block"
+            , style_ "height:150px"
+            , hxGet_ $ "/charts_html?id=reqsChartsErr&theme=roma&show_legend=true&pid=" <> page.pid.toText
+            , hxTrigger_ "intersect,  htmx:beforeRequest from:#log_explorer_form"
+            , hxVals_ "js:{query_raw:window.getQueryFromEditor('errors'), since: getTimeRange().since, from: getTimeRange().from, to:getTimeRange().to, cols:params().cols, layout:'all', source: params().source}"
+            , hxSwap_ "innerHTML"
+            ]
+            ""
+          div_
+            [ id_ "reqsChartsLat"
+            , class_ "px-5 hidden group-has-[.toggle-chart:checked]/result:block"
+            , style_ "height:150px"
+            , hxGet_ $ "/charts_html?id=reqsChartsLat&chart_type=LineCT&show_legend=true&pid=" <> page.pid.toText
+            , hxTrigger_ "intersect,  htmx:beforeRequest from:#log_explorer_form"
+            , hxVals_ "js:{query_raw:window.getQueryFromEditor('latency'), since: getTimeRange().since, from: getTimeRange().from, to:getTimeRange().to, cols:params().cols, layout:'all', source: params().source}"
+            , hxSwap_ "innerHTML"
+            ]
+            ""
       resultTableAndMeta_ page
   jsonTreeAuxillaryCode page.pid
   -- drawerWithURLContent_ : Used when you expand a log item
