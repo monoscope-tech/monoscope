a6de98bbde12d27221aedb2095ec9d7127e06139
dawkaka <yousiph77@gmail.com>
2024-11-02 21:43:52 +0000

    endpoint link

--- src/Pages/LogExplorer/LogItem.hs
+++ src/Pages/LogExplorer/LogItem.hs
@@ -24,7 +24,7 @@ import Pages.Traces.Spans qualified as Spans
 import PyF (fmt)
 import Relude
 import System.Types (ATAuthCtx, RespHeaders, addRespHeaders)
-import Utils (faSprite_, getMethodColor, getStatusColor, jsonValueToHtmlTree)
+import Utils (faSprite_, getMethodColor, getStatusColor, jsonValueToHtmlTree, toXXHash)
 import Witch (from)
 
 
@@ -95,6 +95,8 @@ expandAPIlogItem' pid req modal = do
                   ]
                   "Get link"
     -- url, endpoint, latency, request size, repsonse size
+    let endpointHash = toXXHash $ pid.toText <> req.host <> req.method <> req.urlPath
+    let endpointURl = "/p/" <> pid.toText <> "/log_explorer/endpoint/" <> endpointHash
     div_ [class_ "flex flex-col mt-4 justify-between w-full"] do
       div_ [class_ "text-base mb-2 flex gap-6 items-center"] do
         span_ [class_ "text-slate-500 font-medium w-16"] "Endpoint"
@@ -102,14 +104,16 @@ expandAPIlogItem' pid req modal = do
           span_ [class_ "text-slate-800 text-sm truncate ellipsis urlPath", term "data-tippy" req.urlPath] $ toHtml req.urlPath
           div_ [[__| install Copy(content:.urlPath )|]] do
             faSprite_ "copy" "regular" "h-8 w-8 border border-slate-300 bg-slate-100 rounded-full p-2 text-slate-500"
-          faSprite_ "arrow-up-right" "regular" "h-8 w-8 p-2 blue-gr-btn rounded-full"
+          a_ [href_ endpointURl] do
+            faSprite_ "arrow-up-right" "regular" "h-8 w-8 p-2 blue-gr-btn rounded-full"
       div_ [class_ "text-base flex items-center gap-6"] do
         span_ [class_ "text-slate-500 font-medium w-16"] "URL"
         div_ [class_ "flex gap-1 items-center"] do
           span_ [class_ "text-slate-800 text-sm truncate ellipsis", term "data-tippy" req.rawUrl] $ toHtml req.rawUrl
           div_ [[__| install Copy(content:.urlPath )|]] do
             faSprite_ "copy" "regular" "h-8 w-8 border border-slate-300 bg-slate-100 rounded-full p-2 text-slate-500"
-          faSprite_ "arrow-up-right" "regular" "h-8 w-8 p-2 blue-gr-btn rounded-full"
+          a_ [href_ endpointURl] do
+            faSprite_ "arrow-up-right" "regular" "h-8 w-8 p-2 blue-gr-btn rounded-full"
       div_ [class_ "flex gap-2 mt-4"] do
         statBox_ Nothing (Just ("clock", "regular", "text-blue-500")) "Latency" "Latency" (show (req.durationNs `div` 1000) <> " ms") Nothing Nothing
         let reqSize = BS.length $ AE.encode req.requestBody
