62da4d2a8594903fab81833e20f5bd2565b96418
Daniel Asaboro <asaborodaniel@gmail.com>
2024-11-27 11:25:14 +0100

    fix: made scroll behaviour smooth instead of the revert to instant

--- src/Pages/BodyWrapper.hs
+++ src/Pages/BodyWrapper.hs
@@ -236,6 +236,7 @@ bodyWrapper BWConfig{sessM, currProject, pageTitle, menuItem, hasIntegrated, nav
         var currentISOTimeStringVar = ((new Date()).toISOString().split(".")[0])+"+00:00";
         document.addEventListener('DOMContentLoaded', function(){
           // htmx.config.useTemplateFragments = true
+          htmx.config.scrollBehavior = 'smooth'
           tippy('[data-tippy-content]');
           var notyf = new Notyf({
               duration: 5000,
