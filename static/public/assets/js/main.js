7c48b6d246b46e1f669c9ac5ce9d219d820aed52
dawkaka <yousiph77@gmail.com>
2025-02-13 10:24:11 +0000

    fix error on tab switch with queryAST search param

--- static/public/assets/js/main.js
+++ static/public/assets/js/main.js
@@ -1,42 +1,45 @@
 // buildCurlRequest converts a log explorer result item into a curl and copies the curl to clipboard.
-window.buildCurlRequest = function(event) {
-  const { request_headers, request_body, method, host, raw_url } = JSON.parse(event.currentTarget.dataset.reqjson);
-  let curlCommand = `curl -X ${method} https://${host}${raw_url} \\\n `;
+window.buildCurlRequest = function (event) {
+  const { request_headers, request_body, method, host, raw_url } = JSON.parse(event.currentTarget.dataset.reqjson)
+  let curlCommand = `curl -X ${method} https://${host}${raw_url} \\\n `
 
-  const curlHeaders = typeof request_headers === 'object'
-    ? Object.entries(request_headers).map(([key, value]) => `-H "${key} ${value}" \\\n`).join('')
-    : '';
-  curlCommand += curlHeaders;
+  const curlHeaders =
+    typeof request_headers === 'object'
+      ? Object.entries(request_headers)
+          .map(([key, value]) => `-H "${key} ${value}" \\\n`)
+          .join('')
+      : ''
+  curlCommand += curlHeaders
 
-  const reqBody = method.toLowerCase() !== 'get'
-    ? (typeof request_body === 'object'
-      ? ` -d '${JSON.stringify(request_body)}' \\\n`
-      : `-data-raw "${request_body}"  \\\n`)
-    : '';
-  if (reqBody) curlCommand += reqBody;
+  const reqBody = method.toLowerCase() !== 'get' ? (typeof request_body === 'object' ? ` -d '${JSON.stringify(request_body)}' \\\n` : `-data-raw "${request_body}"  \\\n`) : ''
+  if (reqBody) curlCommand += reqBody
 
   navigator.clipboard.writeText(curlCommand).then(() => {
-    document.querySelector('body').dispatchEvent(new CustomEvent('successToast', {
-      detail: { value: ['Curl command copied'] },
-      bubbles: true,
-      composed: true,
-    }));
-  });
-};
+    document.querySelector('body').dispatchEvent(
+      new CustomEvent('successToast', {
+        detail: { value: ['Curl command copied'] },
+        bubbles: true,
+        composed: true,
+      })
+    )
+  })
+}
 
 window.setQueryParamAndReload = (key, value) => {
   const url = new URL(window.location.href)
   url.searchParams.set(key, value)
-  url.searchParams.delete('query')
-  url.searchParams.delete('target-spans')
+  if (key === 'source') {
+    url.searchParams.delete('queryAST')
+    url.searchParams.delete('target-spans')
+  }
   window.location.href = url.toString()
 }
 
 // TODO: implement correctly. errors doesnt work
 window.getQueryFromEditor = (target) => {
   const toggler = document.getElementById('toggleQueryEditor')
-  const queryAST = document.getElementById('filterElement').ast;
-  console.log(queryAST, "getQueryFromEditor", target, )
+  const queryAST = document.getElementById('filterElement').ast
+  console.log(queryAST, 'getQueryFromEditor', target)
   let val = ''
   if (toggler.checked) {
     val = window.editor.getValue()
@@ -52,7 +55,7 @@ window.getQueryFromEditor = (target) => {
   return val
 }
 
-window.downloadJson = function(event) {
+window.downloadJson = function (event) {
   event.stopPropagation()
   const json = event.currentTarget.dataset.reqjson
   var blob = new Blob([json], { type: 'application/json' })
@@ -65,13 +68,13 @@ window.downloadJson = function(event) {
   document.body.removeChild(a)
 }
 
-window.evalScriptsFromContent = function(container) {
+window.evalScriptsFromContent = function (container) {
   container.querySelectorAll('script').forEach((oldScript) => {
     const newScript = document.createElement('script')
     newScript.text = oldScript.textContent || oldScript.innerHTML
 
-      // Copy attributes using the spread operator
-      ;[...oldScript.attributes].forEach((attr) => newScript.setAttribute(attr.name, attr.value))
+    // Copy attributes using the spread operator
+    ;[...oldScript.attributes].forEach((attr) => newScript.setAttribute(attr.name, attr.value))
 
     // Append and remove to execute
     document.body.append(newScript)
@@ -85,7 +88,7 @@ var params = () =>
   })
 window.params = params
 
-var getTimeRange = function() {
+var getTimeRange = function () {
   const rangeInput = document.getElementById('custom_range_input')
   const range = rangeInput.value.split('/')
   if (range.length == 2) {
@@ -123,9 +126,10 @@ var removeNamedColumnToSummary = (namedCol) => {
 // Example usage
 // setParams({ param1: 'newValue1' }, true); // Will trigger a full page load with new state
 // setParams({ param2: 'newValue2' });       // Will update URL without reloading
-window.setParams = ((state = { ...Object.fromEntries(new URLSearchParams(window.location.search)) }) =>
+window.setParams = (
+  (state = { ...Object.fromEntries(new URLSearchParams(window.location.search)) }) =>
   (newState, load = false) => {
-    Object.assign(state, newState);
+    Object.assign(state, newState)
 
     const url =
       '?' +
@@ -133,40 +137,44 @@ window.setParams = ((state = { ...Object.fromEntries(new URLSearchParams(window.
         Object.entries(state)
           .filter(([_key, value]) => value != null)
           .sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
-      ).toString();
+      ).toString()
 
-    load ? window.location.assign(url) : history.replaceState(null, '', url);
+    load ? window.location.assign(url) : history.replaceState(null, '', url)
   }
-)();
+)()
 
 function updateMarkAreas(chartId, warningVal, incidentVal) {
   const warning = parseInt(warningVal, 10),
     incident = parseInt(incidentVal, 10),
     myChart = echarts.getInstanceByDom(document.getElementById(chartId)),
-    options = myChart.getOption();
+    options = myChart.getOption()
 
-  options.series.forEach(series => {
+  options.series.forEach((series) => {
     series.markArea = {
       label: { show: false },
       data: [
-        ...(!isNaN(warning) ? [[
-          {
-            name: 'Warning',
-            yAxis: warning,
-            itemStyle: { color: 'rgba(255, 212, 0, 0.4)' }
-          },
-          { yAxis: incident }
-        ]] : []),
+        ...(!isNaN(warning)
+          ? [
+              [
+                {
+                  name: 'Warning',
+                  yAxis: warning,
+                  itemStyle: { color: 'rgba(255, 212, 0, 0.4)' },
+                },
+                { yAxis: incident },
+              ],
+            ]
+          : []),
         [
           {
             name: 'Incident',
             yAxis: incident,
-            itemStyle: { color: 'rgba(255, 173, 177, 0.5)' }
+            itemStyle: { color: 'rgba(255, 173, 177, 0.5)' },
           },
-          { yAxis: 'max' }
-        ]
-      ]
-    };
-  });
-  myChart.setOption({ series: options.series }, false);
+          { yAxis: 'max' },
+        ],
+      ],
+    }
+  })
+  myChart.setOption({ series: options.series }, false)
 }
