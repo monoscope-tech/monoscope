e957e9f2ea351c4326cdc98a1ade85266ae0c3d6
dawkaka <yousiph77@gmail.com>
2025-02-27 12:49:45 +0000

    fix msgs not saved when converted from span to req

--- src/Models/Telemetry/Telemetry.hs
+++ src/Models/Telemetry/Telemetry.hs
@@ -618,12 +618,8 @@ convertSpanToRequestMessage sp instrumentationScope =
     messageId = UUID.fromText $ fromMaybe "" $ getSpanAttribute "apitoolkit.msg_id" sp.attributes
     parentId = UUID.fromText $ fromMaybe "" $ getSpanAttribute "apitoolkit.parent_id" sp.attributes
     referer = Just $ Left (fromMaybe "" $ getSpanAttribute "http.request.headers.referer" sp.attributes) :: Maybe (Either Text [Text])
-    requestBody' = fromMaybe "" $ getSpanAttribute "http.request.body" sp.attributes
-    responseBody' = fromMaybe "" $ getSpanAttribute "http.response.body" sp.attributes
-    reqBodyB64 = B64.decodeBase64Untyped $ Relude.encodeUtf8 requestBody'
-    respBodyB64 = B64.decodeBase64Untyped $ Relude.encodeUtf8 responseBody'
-    requestBody = decodeUtf8 $ fromRight "{}" reqBodyB64
-    responseBody = decodeUtf8 $ fromRight "{}" respBodyB64
+    requestBody = fromMaybe "" $ getSpanAttribute "http.request.body" sp.attributes
+    responseBody = fromMaybe "" $ getSpanAttribute "http.response.body" sp.attributes
     (requestHeaders, responseHeaders) = case sp.attributes of
       AE.Object v -> (getValsWithPrefix "http.request.header." v, getValsWithPrefix "http.response.header." v)
       _ -> (AE.object [], AE.object [])
