e5c9170dc634a5a85f2ddba66252a0ae1ec038f3
Anthony Alaribe <anthonyalaribe@gmail.com>
2025-03-13 19:54:00 +0100

    x

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,39 +36,39 @@ module.exports = {
     'basis-full',
     'w-[16ch] ',
   ],
-  daisyui: {
-    themes: [
-      'winter',
-      'fantasy',
-      'cmyk',
-      'pastel',
-      'emerald',
-      {
-        antdtheme: {
-          'color-scheme': 'light',
-          primary: '#1677ff', // Ant Design primary color
-          'primary-content': '#ffffff', // White text on primary color
-          secondary: '#463AA2', // Ant Design purple
-          accent: '#C148AC',
-          neutral: '#021431',
-          'base-100': '#FBFCFD', // Base background
-          // 'base-200': '#0015800A',
-          'base-300': '#e3e9f4',
-          'base-content': '#394e6a',
-          info: '#1677ff',
-          success: '#81CFD1',
-          warning: '#EFD7BB',
-          error: '#E58B8B',
-          // "--rounded-box": "0.4rem", // 2px border radius
-          // "--rounded-btn": "0.4rem",
-          // "--rounded-badge": "0.4rem",
-          // "--tab-radius": "0.4rem",
-          // "--btn-text-case": "none", // Sentence case for buttons
-          // "--font-family": "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', SimSun, sans-serif;",
-        },
-      },
-    ],
-  },
+  // daisyui: {
+  //   themes: [
+  //     'winter',
+  //     'fantasy',
+  //     'cmyk',
+  //     'pastel',
+  //     'emerald',
+  //     {
+  //       antdtheme: {
+  //         'color-scheme': 'light',
+  //         primary: '#1677ff', // Ant Design primary color
+  //         'primary-content': '#ffffff', // White text on primary color
+  //         secondary: '#463AA2', // Ant Design purple
+  //         accent: '#C148AC',
+  //         neutral: '#021431',
+  //         'base-100': '#FBFCFD', // Base background
+  //         // 'base-200': '#0015800A',
+  //         'base-300': '#e3e9f4',
+  //         'base-content': '#394e6a',
+  //         info: '#1677ff',
+  //         success: '#81CFD1',
+  //         warning: '#EFD7BB',
+  //         error: '#E58B8B',
+  //         // "--rounded-box": "0.4rem", // 2px border radius
+  //         // "--rounded-btn": "0.4rem",
+  //         // "--rounded-badge": "0.4rem",
+  //         // "--tab-radius": "0.4rem",
+  //         // "--btn-text-case": "none", // Sentence case for buttons
+  //         // "--font-family": "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', SimSun, sans-serif;",
+  //       },
+  //     },
+  //   ],
+  // },
   theme: {
     extend: {
       colors: {
@@ -389,5 +389,6 @@ module.exports = {
       },
     },
   },
-  plugins: [require('daisyui'), require('@tailwindcss/typography')],
+  // plugins: [require('daisyui'), require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography')],
 }
