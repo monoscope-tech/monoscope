3580a93113cd305ab5e56ac1a2b898775ca2a258
dawkaka <yousiph77@gmail.com>
2025-03-24 10:09:32 +0000

    s

--- src/Pages/Log.hs
+++ src/Pages/Log.hs
@@ -486,7 +486,7 @@ apiLogsPage :: ApiLogsPageData -> Html ()
 apiLogsPage page = do
   section_ [class_ "mx-auto pt-2 px-6 gap-3.5 w-full flex flex-col h-full overflow-hidden pb-2  group/pg", id_ "apiLogsPage"] do
     template_ [id_ "loader-tmp"] $ span_ [class_ "loading loading-dots loading-md"] ""
-    when True $ freeTierLimitExceededBanner page.pid.toText
+    when page.exceededFreeTier $ freeTierLimitExceededBanner page.pid.toText
     div_
       [ style_ "z-index:26"
       , class_ "fixed hidden right-0 top-0 justify-end left-0 bottom-0 w-full bg-black bg-opacity-5"
