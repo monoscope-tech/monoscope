d293de10edb88a7ab32c9ce18c6ecdc3972748e1
dawkaka <yousiph77@gmail.com>
2025-03-30 20:03:12 +0000

    remove from project dropdown

--- src/Pages/BodyWrapper.hs
+++ src/Pages/BodyWrapper.hs
@@ -270,8 +270,8 @@ bodyWrapper bcfg child = do
         , tabindex_ "-1"
         ]
         do
-          div_ [class_ "relative mx-auto max-h-full", style_ "width: min(90vw, 500px)"]
-            $ div_ [class_ "bg-base-100 rounded-lg drop-shadow-md border-1 w-full"] do
+          div_ [class_ "relative mx-auto max-h-full", style_ "width: min(90vw, 500px)"] $
+            div_ [class_ "bg-base-100 rounded-lg drop-shadow-md border-1 w-full"] do
               div_ [class_ "flex items-start justify-between p-6 space-x-2  border-b rounded-t"] do
                 h3_ [class_ "text-3xl font-bold "] "Only Desktop Browsers are Supported for now!"
               -- Modal body
@@ -283,17 +283,17 @@ bodyWrapper bcfg child = do
               div_ [class_ "flex w-full justify-end items-center p-6 space-x-2 border-t border-gray-200 rounded-b"] pass
       case bcfg.sessM of
         Nothing -> do
-          section_ [class_ "flex flex-col grow  h-screen overflow-y-hidden"]
-            $ section_ [class_ "flex-1 overflow-y-auto"]
-            $ child
+          section_ [class_ "flex flex-col grow  h-screen overflow-y-hidden"] $
+            section_ [class_ "flex-1 overflow-y-auto"] $
+              child
         Just sess ->
           let currUser = sess.persistentSession.user.getUser
               sideNav' = bcfg.currProject & maybe "" \project -> sideNav sess project (fromMaybe bcfg.pageTitle bcfg.prePageTitle) bcfg.menuItem bcfg.hasIntegrated
            in section_ [class_ "flex flex-row grow-0 h-screen overflow-hidden"] do
                 sideNav'
                 section_ [class_ "h-screen overflow-y-hidden grow"] do
-                  when (currUser.email == "hello@apitoolkit.io")
-                    $ loginBanner
+                  when (currUser.email == "hello@apitoolkit.io") $
+                    loginBanner
                   navbar bcfg.currProject (fromMaybe [] (bcfg.currProject <&> \p -> menu p.id)) currUser bcfg.prePageTitle bcfg.pageTitle bcfg.pageTitleModalId bcfg.docsLink bcfg.navTabs bcfg.pageActions
                   section_ [class_ "overflow-y-hidden h-full flex-1"] child
                   Components.drawer_ "global-data-drawer" Nothing Nothing ""
@@ -350,16 +350,10 @@ projectsDropDown currProject projects = do
             strong_ [class_ "block"] $ toHtml currProject.title
             small_ [class_ "block"] $ toHtml currProject.paymentPlan
         nav_ [] do
-          a_ [href_ [text| /p/$pidTxt/settings |], class_ "p-3 flex gap-3 items-center rounded-2xl hover:bg-gray-100"] do
-            faSprite_ "gear" "regular" "h-5 w-5" >> span_ "Settings"
-          a_ [href_ [text| /p/$pidTxt/manage_members |], class_ "p-3 flex gap-3 items-center rounded-sm hover:bg-gray-100"] do
-            faSprite_ "user-plus" "regular" "h-5 w-5" >> span_ "Manage members"
-          a_ [href_ [text| /p/$pidTxt/apis|], class_ "p-3 flex gap-3 items-center rounded-sm hover:bg-gray-100"] do
-            faSprite_ "key" "regular" "h-5 w-5" >> span_ "API Keys"
           a_ [href_ [text| /p/$pidTxt/integrations|], class_ "p-3 flex gap-3 items-center rounded-sm hover:bg-gray-100"] do
             faSprite_ "arrows-turn-right" "regular" "h-5 w-5" >> span_ "Integrations"
-          when (currProject.paymentPlan == "UsageBased" || currProject.paymentPlan == "GraduatedPricing")
-            $ a_
+          when (currProject.paymentPlan == "UsageBased" || currProject.paymentPlan == "GraduatedPricing") $
+            a_
               [class_ "p-3 flex gap-3 items-center rounded-sm hover:bg-gray-100 cursor-pointer", hxGet_ [text| /p/$pidTxt/manage_subscription |]]
               (faSprite_ "dollar-sign" "regular" "h-5 w-5" >> span_ "Manage billing")
       div_ [class_ "border-t border-gray-100 p-2"] do
@@ -417,6 +411,8 @@ sideNav sess project pageTitle menuItem hasIntegrated = aside_ [class_ "border-r
             span_ [class_ "hidden group-has-[#sidenav-toggle:checked]/pg:block whitespace-nowrap truncate"] $ toHtml mTitle
 
   div_ [class_ "py-8 px-2 group-has-[#sidenav-toggle:checked]/pg:px-6 *:gap-2 *:whitespace-nowrap *:truncate flex flex-col gap-2.5 *:items-center *:overflow-x-hidden *:flex &:no-wrap"] do
+    mapM_ renderNavBottomItem $ navBottomList project.id.toText
+
     let currUser = sess.persistentSession.user.getUser
     let userIdentifier =
           if currUser.firstName /= "" || currUser.lastName /= ""
@@ -426,30 +422,12 @@ sideNav sess project pageTitle menuItem hasIntegrated = aside_ [class_ "border-r
     let sanitizedID = T.replace " " "+" userIdentifier
     div_ [tabindex_ "0", role_ "button", class_ "cursor-pointer justify-center group-has-[#sidenav-toggle:checked]/pg:justify-start"] do
       img_
-        [ class_ "inline-block w-10 h-10 p-2 rounded-full bg-gray-300"
+        [ class_ "inline-block w-8 h-8 p-1 rounded-full bg-gray-300"
         , term "data-tippy-placement" "right"
         , term "data-tippy-content" userIdentifier
         , src_ [text|https://www.gravatar.com/avatar/${emailMd5}?d=https%3A%2F%2Fui-avatars.com%2Fapi%2F/${sanitizedID}/128|]
         ]
       span_ [class_ "hidden group-has-[#sidenav-toggle:checked]/pg:inline-block overflow-hidden"] $ toHtml userIdentifier
-    a_
-      [ class_ "hover:bg-blue-50 "
-      , target_ "blank"
-      , term "data-tippy-placement" "right"
-      , term "data-tippy-content" "Documentation"
-      , href_ "https://apitoolkit.io/docs/"
-      ]
-      $ span_ [class_ "p-3 rounded-full bg-blue-100 text-brand leading-none"] (faSprite_ "circle-question" "regular" "h-4 w-4")
-      >> span_ [class_ "hidden group-has-[#sidenav-toggle:checked]/pg:block"] "Documentation"
-    a_
-      [ class_ "hover:bg-blue-50"
-      , term "data-tippy-placement" "right"
-      , term "data-tippy-content" "Logout"
-      , href_ "/logout"
-      , [__| on click js posthog.reset(); end |]
-      ]
-      $ span_ [class_ "p-3 rounded-full bg-red-100 text-red-600 leading-none"] (faSprite_ "arrow-right-from-bracket" "regular" "h-4 w-4")
-      >> span_ [class_ "hidden group-has-[#sidenav-toggle:checked]/pg:block"] "Logout"
 
 
 navbar :: Maybe Projects.Project -> [(Text, Text, Text)] -> Users.User -> Maybe Text -> Text -> Maybe Text -> Maybe Text -> Maybe (Html ()) -> Maybe (Html ()) -> Html ()
@@ -501,3 +479,37 @@ loginBanner = do
     a_ [class_ "underline underline-offset-2 ", href_ "https://apitoolkit.io/docs/onboarding/"] "Documentation"
     a_ [class_ "py-2 px-3 rounded-xl bg-transparent border border-fillBrand-strong text-fillBrand-strong shadow-sm hover:shadow-md", href_ "https://calendar.app.google/1a4HG5GZYv1sjjZG6"] "Book a demo with an engineer"
     a_ [class_ "py-2 px-3 rounded-xl bg-fillBrand-strong text-textInverse-strong shadow-sm hover:shadow-md", href_ "/login"] "Start 30 day free trial"
+
+
+renderNavBottomItem :: (Text, Text, Text, Text, Text, Maybe Text, Maybe Text) -> Html ()
+renderNavBottomItem (iconName, bgColor, textColor, linkText, link, targetBlankM, onClickM) =
+  let
+    defaultAttrs =
+      [ class_ $ "hover:bg-blue-50"
+      , term "data-tippy-placement" "right"
+      , term "data-tippy-content" linkText
+      , href_ link
+      ]
+
+    attrs =
+      defaultAttrs
+        ++ (if isJust targetBlankM then [target_ "BLANK_"] else [])
+        ++ (maybe [] (\onClick -> [[__|hxGet_ onClick|]]) onClickM)
+   in
+    a_ attrs $ do
+      span_
+        [class_ $ "p-2 rounded-full " <> bgColor <> " " <> textColor <> " leading-none"]
+        (faSprite_ iconName "regular" "h-3 w-3")
+      span_
+        [class_ "hidden group-has-[#sidenav-toggle:checked]/pg:block"]
+        (toHtml linkText)
+
+
+navBottomList :: Text -> [(Text, Text, Text, Text, Text, Maybe Text, Maybe Text)]
+navBottomList pidTxt =
+  [ ("gear", "bg-blue-100", "text-brand", "Settings", "/p/" <> pidTxt <> "/settings", Nothing, Nothing)
+  , ("key", "bg-green-100", "text-green-600", "API Keys", "/p/" <> pidTxt <> "/apis", Nothing, Nothing)
+  , ("user-plus", "bg-yellow-100", "text-yellow-600", "Manage members", "/p/" <> pidTxt <> "/manage_members", Nothing, Nothing)
+  , ("circle-question", "bg-blue-100", "text-brand", "Documentation", "https://apitoolkit.io/docs/", Just "blank", Nothing)
+  , ("arrow-right-from-bracket", "bg-red-100", "text-red-600", "Logout", "/logout", Nothing, Just "posthog.reset()")
+  ]
