537d579cca2785e860496cae6dc029202c51889e
dawkaka <yousiph77@gmail.com>
2025-04-28 10:40:13 +0000

    project settings

--- src/Pages/Projects/CreateProject.hs
+++ src/Pages/Projects/CreateProject.hs
@@ -377,64 +377,65 @@ processProjectPostForm cpRaw pid = do
 -- createProjectBody is the core html view
 createProjectBody :: Sessions.PersistentSession -> Projects.ProjectId -> EnvConfig -> CreateProjectForm -> CreateProjectFormError -> Html ()
 createProjectBody sess pid envCfg cp cpe = do
-  section_ [id_ "main-content", class_ "p-3 py-5 sm:p-6 overflow-y-scroll h-full"] do
-    div_ [class_ "mx-auto", style_ "max-width:800px"] do
+  section_ [id_ "main-content", class_ "overflow-y-scroll h-full text-textWeak"] do
+    div_ [class_ "mx-auto px-2 pt-18", style_ "max-width:800px"] do
       h2_ [class_ "text-slate-700 text-3xl font-medium mb-5"] "Project Settings"
-      div_ [class_ "grid gap-5"] do
-        form_
-          [ class_ "col-span-1 relative px-3 sm:px-10 border border-gray-200 py-10  bg-base-100 rounded-3xl"
-          , hxPost_ $ "/p/update/" <> pid.toText
-          , hxTarget_ "#main-content"
-          , hxSwap_ "outerHTML"
-          , id_ "createUpdateBodyForm"
-          , hxIndicator_ "#createIndicator"
-          ]
-          do
-            div_ do
-              label_ [class_ " font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"] do
-                "Title"
-                span_ [class_ "text-red-400"] " *"
-              input_
-                [ class_ "flex h-9 w-full rounded-lg border border-input bg-transparent px-3 py-1  shadow-xs transition-colors placeholder:text-muted-foreground focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring"
-                , type_ "text"
-                , id_ "title"
-                , name_ "title"
-                , value_ cp.title
-                , required_ "required"
-                ]
-            div_ [class_ "flex flex-col gap-1 mt-5"] do
-              label_ [class_ " font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"] do
-                "Timezone"
-              select_ [name_ "timeZone", id_ "timezone", class_ "px-4 py-2 border bg-gray-100 rounded-lg"] do
-                option_ [value_ cp.timeZone] $ toHtml cp.timeZone
-            div_ [class_ "mt-5 "] do
-              label_ [class_ " font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"] "Description"
-              textarea_
-                [ class_ " flex min-h-[60px] w-full rounded-lg border border-input bg-transparent px-3 py-2  shadow-xs placeholder:text-muted-foreground focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 "
-                , rows_ "4"
-                , placeholder_ "Description"
-                , id_ "description"
-                , name_ "description"
-                ]
-                $ toHtml cp.description
+      form_
+        [ class_ "py-8 flex flex-col gap-8 w-full"
+        , hxPost_ $ "/p/update/" <> pid.toText
+        , hxTarget_ "#main-content"
+        , hxSwap_ "outerHTML"
+        , id_ "createUpdateBodyForm"
+        , hxIndicator_ "#createIndicator"
+        ]
+        do
+          div_ [class_ "flex flex-col gap-1 w-full"] do
+            label_ [class_ "text-textStrong leading-normal"] do
+              "Project title"
+              span_ [class_ ""] " *"
+            input_
+              [ class_ "input w-full"
+              , type_ "text"
+              , id_ "title"
+              , name_ "title"
+              , value_ cp.title
+              , required_ "required"
+              ]
+          div_ [class_ "flex flex-col gap-1 w-full"] do
+            label_ [class_ "text-textStrong leading-normal"] do
+              "Timezone"
+            select_ [name_ "timeZone", id_ "timezone", class_ "select w-full"] do
+              option_ [value_ cp.timeZone] $ toHtml cp.timeZone
+          div_ [class_ "flex flex-col gap-1 w-full"] do
+            label_ [class_ "text-textStrong leading-normal"] "Description"
+            textarea_
+              [ class_ "textarea w-full"
+              , rows_ "4"
+              , placeholder_ "Description"
+              , id_ "description"
+              , name_ "description"
+              ]
+              $ toHtml cp.description
 
-            -- LEMON SQUEEZY PAYMENT
+          div_ [class_ "flex w-full justify-end items-center"] do
+            -- a_ [href_ $ "/p/" <> pid.toText <> "/update_pricing", class_ "text-textBrand font-medium"] "Update pricing"
+            button_
+              [ class_ "lemonsqueezy-button py-2 px-5 w-max bg-blue-700 flex items-center text-[white]  rounded-xl cursor-pointer"
+              ]
+              do
+                span_ [id_ "createIndicator", class_ "htmx-indicator loading loading-dots loading-md"] ""
+                "Update project"
 
-            div_ [class_ "p-5 flex w-full justify-between items-center"] do
-              a_ [href_ $ "/p/" <> pid.toText <> "/update_pricing", class_ "text-textBrand font-medium"] "Update pricing"
-
-              button_
-                [ class_ "lemonsqueezy-button py-2 px-5 w-max bg-blue-700 flex items-center text-[white]  rounded-xl cursor-pointer"
-                ]
-                do
-                  span_ [id_ "createIndicator", class_ "htmx-indicator loading loading-dots loading-md"] ""
-                  "Proceed"
+      -- div_ [class_ "border-t py-8"] do
+      --   div_ [class_ "self-stretch justify-start text-textStrong text-base font-semibold font-['Inter']"] "Upgrade plan"
+      --   p_ [class_ "text-textWeak text-sm max-w-[500px]"] "This is APItoolkit pricing, click on compare feature below to select the option that best suit your project."
 
       let pidText = pid.toText
-      div_ [class_ "col-span-1 h-full justify-center items-center w-full text-center pt-24"] do
-        h2_ [class_ "text-red-800 font-medium pb-4"] "Delete project. This is dangerous and unreversible."
+      div_ [class_ "border border-red-500 gap-5 w-full p-12 mt-24 rounded-lg bg-red-50"] do
+        h1_ [class_ "text-red-500 font-medium text-xl"] "Danger zone"
+        p_ [class_ "py-4"] "Delete project. This is dangerous and unreversible."
         button_
-          [ class_ "btn btn-sm bg-red-800 text-white shadow-md hover:bg-red-700 cursor-pointer rounded-md"
+          [ class_ "btn btn-sm bg-red-500 text-white shadow-md hover:bg-red-700 cursor-pointer"
           , hxGet_ [text|/p/$pidText/delete|]
           , hxConfirm_ "Are you sure you want to delete this project?"
           ]
