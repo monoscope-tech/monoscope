5ae8ba1573e48547183c7040956b329403c2ddbf
= <=>
2025-05-19 01:47:37 +0100

    test refactorings

--- src/Pkg/ParserSpec.hs
+++ src/Pkg/ParserSpec.hs
@@ -26,34 +26,19 @@ spec = do
       let (query, _) = fromRight' $ parseQueryToComponents (defSqlQueryCfg defPid fixedUTCTime Nothing Nothing) "method==\"GET\""
       let expected =
             [text|
-      SELECT json_build_array(id::text,
-      to_char(created_at AT TIME ZONE 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS.US"Z"'),
-      request_type,host,status_code,method,url_path,JSONB_ARRAY_LENGTH(errors),
-      LEFT( CONCAT(
-        'url=', COALESCE(raw_url, 'null'),
-        ' response_body=', COALESCE(response_body, 'null'),
-        ' request_body=', COALESCE(request_body, 'null')
-      ), 255
-      )) FROM apis.request_dumps
-      WHERE project_id='00000000-0000-0000-0000-000000000000'::uuid
-      and ( created_at > NOW() - interval '14 days' AND (method='GET') )
-      ORDER BY created_at desc limit 200|]
+      SELECT json_build_array(id,to_char(timestamp AT TIME ZONE 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS.US"Z"'),context___trace_id,kind,status_message,name,duration,body,level,resource___service___name,parent_id,CAST(EXTRACT(EPOCH FROM (start_time)) * 1_000_000_000 AS BIGINT),EXISTS(SELECT 1 FROM jsonb_array_elements(events) elem WHERE elem->>'event_name' = 'exception'),jsonb_build_object( 'method', COALESCE(attributes->'http'->>'method', attributes___http___request___method), 'url', COALESCE(attributes->'http'->>'route', attributes->'url'->>'path', attributes->'http'->>'target', attributes->'http'->>'url'), 'status_code', COALESCE(attributes->'http'->>'status_code', attributes->'http'->'response'->>'status_code') ),jsonb_build_object('system', attributes->'db'->'system','statement', coalesce(attributes->'db'->'query'->'text', attributes->'db'->'statement')),json_build_object('system', attributes->'rpc'->'system', 'method', attributes->'rpc'->'method'),LEFT( CONCAT( COALESCE(attributes::text, '') ), 500 ),context___span_id) FROM otel_logs_and_spans WHERE project_id='00000000-0000-0000-0000-000000000000' and ( timestamp > NOW() - interval '14 days' AND (method='GET') ) ORDER BY timestamp desc limit 150|]
       normT query `shouldBe` normT expected
     it "timechart query query" do
       let (_, c) = fromRight' $ parseQueryToComponents (defSqlQueryCfg defPid fixedUTCTime Nothing Nothing) "method==\"GET\""
       let expected =
             [text|
-SELECT extract(epoch from time_bucket('1h', created_at))::integer as timeB, count(*)::integer as count, 'Throughput'
-FROM apis.request_dumps WHERE project_id='00000000-0000-0000-0000-000000000000'::uuid
-and ( created_at > NOW() - interval '14 days' AND (method='GET') ) GROUP BY timeB
+SELECT extract(epoch from time_bucket('1h', timestamp))::integer as timeB, count(*)::integer as count, 'Throughput' FROM otel_logs_and_spans WHERE project_id='00000000-0000-0000-0000-000000000000' and ( timestamp > NOW() - interval '14 days' AND (method='GET') ) GROUP BY timeB
       |]
       normT (fromMaybe "" c.finalTimechartQuery) `shouldBe` normT expected
     it "timechart query query 1d" do
       let (_, c) = fromRight' $ parseQueryToComponents (defSqlQueryCfg defPid fixedUTCTime Nothing Nothing) "method==\"GET\" | timechart count(*) [1d]"
       let expected =
             [text|
-SELECT extract(epoch from time_bucket('1d', created_at))::integer as timeB, count(*)::integer as count, 'Throughput'
-FROM apis.request_dumps WHERE project_id='00000000-0000-0000-0000-000000000000'::uuid
-and ( created_at > NOW() - interval '14 days' AND (method='GET') ) GROUP BY timeB
+SELECT extract(epoch from time_bucket('1d', timestamp))::integer as timeB, count(*)::integer as count, 'Throughput' FROM otel_logs_and_spans WHERE project_id='00000000-0000-0000-0000-000000000000' and ( timestamp > NOW() - interval '14 days' AND (method='GET') ) GROUP BY timeB
       |]
       normT (fromMaybe "" c.finalTimechartQuery) `shouldBe` normT expected
