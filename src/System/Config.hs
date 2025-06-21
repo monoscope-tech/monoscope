985e1da29f60f0aa7b0f5095efb013d82e123191
Anthony Alaribe <anthonyalaribe@gmail.com>
2025-06-21 08:16:54 +0200

    reduce projectCache ttl to 30mins

--- src/System/Config.hs
+++ src/System/Config.hs
@@ -136,7 +136,7 @@ configToEnv config = do
   pool <- liftIO $ Pool.newPool $ Pool.defaultPoolConfig createPgConnIO PG.close (60 * 2) 100
   jobsPool <- liftIO $ Pool.newPool $ Pool.defaultPoolConfig createPgConnIO PG.close (60 * 2) 50
   timefusionPgPool <- liftIO $ Pool.newPool $ Pool.defaultPoolConfig createTimefusionPgConnIO PG.close (60 * 2) 50
-  projectCache <- liftIO $ newCache (Just $ TimeSpec (60 * 60) 0) -- :: m (Cache Projects.ProjectId Projects.ProjectCache) -- 60*60secs or 1 hour TTL
+  projectCache <- liftIO $ newCache (Just $ TimeSpec (30 * 60) 0) -- :: m (Cache Projects.ProjectId Projects.ProjectCache) -- 30*60secs or 30 minutes TTL
   projectKeyCache <- liftIO $ newCache Nothing
   pure
     AuthContext
