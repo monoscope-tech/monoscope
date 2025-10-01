a46a118ef312ca16779916bf796e1f9b2f8eef46
Yussif Mohammed <yousiph77@gmail.com>
2025-10-01 18:10:56 +0000

    fix limit clause

--- src/Pkg/Parser.hs
+++ src/Pkg/Parser.hs
@@ -169,7 +169,7 @@ sqlFromQueryComponents sqlCfg qc =
           -- For summarize queries, don't apply a default limit to ensure we get complete data
           case qc.finalSummarizeQuery of
             Just _ -> "" -- No limit for summarize queries unless explicitly specified
-            Nothing -> if (T.null rawWhere) then "limit 150" else "limit 30" -- Default limit for non-summarize queries
+            Nothing -> if (T.null rawWhere) then "limit 30" else "limit 150" -- Default limit for non-summarize queries
 
       -- Create a properly formatted WHERE clause condition
       whereCondition =
