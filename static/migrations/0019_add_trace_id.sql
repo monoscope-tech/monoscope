fcf8ed595c0c5549759f7d000657d53c71c73a28
Yussif Mohammed <yousiph77@gmail.com>
2025-12-23 15:44:51 +0000

    remove concurrently index

--- static/migrations/0019_add_trace_id.sql
+++ static/migrations/0019_add_trace_id.sql
@@ -14,8 +14,7 @@ ALTER TABLE apis.issues
 DROP COLUMN IF EXISTS first_trace_id,
 DROP COLUMN IF EXISTS recent_trace_id;
 
-CREATE INDEX CONCURRENTLY idx_errors_trace_ids ON apis.errors (recent_trace_id, first_trace_id);
-
-
+CREATE INDEX idx_errors_trace_ids
+ON apis.errors (recent_trace_id, first_trace_id);
 
 COMMIT;
