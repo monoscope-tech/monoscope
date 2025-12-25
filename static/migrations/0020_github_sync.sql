42e0c7bc921e19ba60d7f1e5d7e6bd38f642f29d
Yussif Mohammed <yousiph77@gmail.com>
2025-12-25 16:20:39 +0000

    fix github sync table

--- static/migrations/0020_github_sync.sql
+++ static/migrations/0020_github_sync.sql
@@ -1,10 +1,17 @@
--- After user intsall GitHub app,  table for storing installation info, it should be linked to a project 
+BEGIN;
+
 CREATE TABLE IF NOT EXISTS apis.github_installations (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    installation_id BIGINT UNIQUE NOT NULL,
-    project_id INT REFERENCES projects.projects(id) ON DELETE CASCADE,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    installation_id BIGINT NOT NULL UNIQUE,
+    project_id UUID NOT NULL UNIQUE
+        REFERENCES projects.projects(id) ON DELETE CASCADE,
     access_token TEXT,
-    token_expires_at TIMESTAMP,
+    token_expires_at TIMESTAMPTZ
 );
+
+SELECT manage_updated_at('apis.github_installations');
+CREATE INDEX idx_github_installations_project_id ON apis.github_installations(project_id);
+
+COMMIT;
