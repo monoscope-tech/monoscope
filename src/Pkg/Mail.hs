2d7d03fc027a5bbfff537b3701011e0c38d0489f
Yussif Mohammed <yousiph77@gmail.com>
2026-02-22 13:49:07 +0000

    feat: use webhook url for slack notifications instead of channel id

--- src/Pkg/Mail.hs
+++ src/Pkg/Mail.hs
@@ -42,7 +42,7 @@ sendSlackMessage pid message = do
   case slackData of
     Just s -> do
       let payload = [aesonQQ| {"text": #{message}, "type":"mrkdwn"} |]
-      Notify.sendNotification $ Notify.slackNotification s.webhookUrl payload
+      Notify.sendNotification $ Notify.slackNotification s.channelId s.webhookUrl payload
     Nothing -> Log.logAttention "sendSlackMessage is not configured. But was called" (pid, message)
 
 
@@ -85,8 +85,10 @@ sendDiscordAlert alert pid pTitle channelIdM' = do
 sendSlackAlert :: (DB es, Notify.Notify :> es, Reader Config.AuthContext :> es) => NotificationAlerts -> Projects.ProjectId -> Text -> Maybe Text -> Eff es ()
 sendSlackAlert alert pid pTitle channelM = do
   appCtx <- ask @Config.AuthContext
-  channelIdM <- maybe (getProjectSlackData pid <&> fmap (.channelId)) (pure . Just) channelM
-  for_ channelIdM \cid -> do
+  slackData <- getProjectSlackData pid
+  let channelIdM = channelM <|> fmap (.channelId) slackData
+      webhookUrlM = (.webhookUrl) <$> slackData
+  for_ ((,) <$> channelIdM <*> webhookUrlM) \(cid, wurl) -> do
     let projectUrl = appCtx.env.hostUrl <> "p/" <> pid.toText
         mkAlert = \case
           RuntimeErrorAlert{..} -> Just $ slackErrorAlert errorData pTitle cid projectUrl
@@ -94,7 +96,7 @@ sendSlackAlert alert pid pTitle channelM = do
           ReportAlert{..} -> Just $ slackReportAlert reportType startTime endTime totalErrors totalEvents breakDown pTitle cid reportUrl allChartUrl errorChartUrl
           MonitorsAlert{..} -> Just $ AE.object ["blocks" AE..= AE.Array (V.fromList [AE.object ["type" AE..= "section", "text" AE..= AE.object ["type" AE..= "mrkdwn", "text" AE..= ("🤖 Alert triggered for " <> monitorTitle)]]])]
           ShapeAlert -> Nothing
-    traverse_ (Notify.sendNotification . Notify.slackNotification cid) (mkAlert alert)
+    traverse_ (Notify.sendNotification . Notify.slackNotification cid wurl) (mkAlert alert)
 
 
 sendWhatsAppAlert :: (Notify.Notify :> es, Reader Config.AuthContext :> es) => NotificationAlerts -> Projects.ProjectId -> Text -> V.Vector Text -> Eff es ()
