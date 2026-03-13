c56a5b4a39cef298695dd31fdd3719fd08ae16ff
Anthony Alaribe <anthonyalaribe@gmail.com>
2026-03-13 14:55:31 +0100

    Remove unused QP and QPB type aliases

--- src/Web/Routes.hs
+++ src/Web/Routes.hs
@@ -121,8 +121,6 @@ type GetRedirect = Verb 'GET 302
 -- Query parameter types
 type QPT a = QueryParam a Text
 type QPU a = QueryParam a UTCTime
-type QP a b = QueryParam a b
-type QPB a = QueryParam a Bool
 type QPI a = QueryParam a Int
 type QEID a = QueryParam a Endpoints.EndpointId
 type QPUUId a = QueryParam a UUID.UUID
