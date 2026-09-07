ae571327309624df6c020150b3fea22f6b07ac0b
Anthony Alaribe <anthonyalaribe@gmail.com>
2026-09-07 15:28:18 +0200

    Reconcile local work, update monitor recipients and compact chart retry control

--- src/Pkg/Components/Widget.hs
+++ src/Pkg/Components/Widget.hs
@@ -942,7 +942,7 @@ renderChart widget = do
             $ div_ [class_ "flex items-center gap-1.5 text-xs text-textError"] do
               Utils.faSprite_ "circle-exclamation" "solid" "w-3 h-3 shrink-0"
               span_ [class_ "min-w-0 break-words", id_ $ chartId <> "_errorMsg", role_ "status"] ""
-              button_ [type_ "button", id_ $ chartId <> "_retry", hidden_ "", class_ "shrink-0 underline px-2 py-1 disabled:opacity-50"] "Retry"
+              button_ [type_ "button", id_ $ chartId <> "_retry", hidden_ "", class_ "shrink-0 underline px-2 disabled:opacity-50"] "Retry"
           whenJust ((,) <$> widget.groupByOptions <*> widget.groupByUrl) \(options, url) ->
             let selectedLabel = fromMaybe "All values" $ guarded (/= "all") =<< widget.groupBySelected
              in div_ [class_ "flex shrink-0 justify-end px-3 pt-3"]
